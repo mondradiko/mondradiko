784f48603555362f9a3424eebe6d5d0628ca3789
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-05 01:10:56 -0600

    Add XrDisplay findSystem and createSession

--- src/xr/XrDisplay.cpp
+++ src/xr/XrDisplay.cpp
@@ -1,8 +1,19 @@
 #include "xr/XrDisplay.hpp"
 
 #include <cstdio>
+#include <vector>
+
+#include <vulkan/vulkan.h>
 
 #include "log/log.hpp"
+#include "graphics/Renderer.hpp"
+
+XrDisplay::~XrDisplay()
+{
+    log_dbg("Cleaning up OpenXR.");
+    if(session != XR_NULL_HANDLE) xrDestroySession(session);
+    if(instance != XR_NULL_HANDLE) xrDestroyInstance(instance);
+}
 
 bool XrDisplay::initialize()
 {
@@ -12,14 +23,35 @@ bool XrDisplay::initialize()
         return false;
     }
 
+    if(!findSystem()) {
+        return false;
+    }
+
     shouldQuit = true;
+
     return true;
 }
 
-void XrDisplay::cleanup()
+bool XrDisplay::createSession(Renderer* renderer)
 {
-    log_dbg("Cleaning up OpenXR.");
-    xrDestroyInstance(instance);
+    log_dbg("Creating OpenXR session.");
+
+    XrGraphicsBindingVulkanKHR vulkanBindings{
+        .type = XR_TYPE_GRAPHICS_BINDING_VULKAN_KHR
+    };
+
+    XrSessionCreateInfo createInfo{
+        .type = XR_TYPE_SESSION_CREATE_INFO,
+        .next = &vulkanBindings,
+        .systemId = systemId
+    };
+
+    if(xrCreateSession(instance, &createInfo, &session) != XR_SUCCESS) {
+        log_err("Failed to create OpenXR session.");
+        return false;
+    }
+
+    return true;
 }
 
 bool XrDisplay::createInstance()
@@ -35,17 +67,40 @@ bool XrDisplay::createInstance()
     sprintf(appInfo.applicationName, "Mondradiko Client");
     sprintf(appInfo.engineName, "Mondradiko");
 
+    std::vector<const char*> extensions{
+        "XR_KHR_vulkan_enable"
+    };
+
+    // TODO Validation layers
     XrInstanceCreateInfo createInfo{
         .type = XR_TYPE_INSTANCE_CREATE_INFO,
         .applicationInfo = appInfo,
         .enabledApiLayerCount = 0,
-        .enabledExtensionCount = 0
+        .enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
+        .enabledExtensionNames = extensions.data()
     };
 
     XrResult result = xrCreateInstance(&createInfo, &instance);
 
     if(result != XR_SUCCESS || instance == nullptr) {
         log_err("Failed to create OpenXR instance. Is an OpenXR runtime running?");
+        return false;
+    }
+
+    return true;
+}
+
+bool XrDisplay::findSystem()
+{
+    log_inf("Choosing OpenXR system.");
+
+    XrSystemGetInfo systemInfo{
+        .type = XR_TYPE_SYSTEM_GET_INFO,
+        .formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY
+    };
+
+    if(xrGetSystem(instance, &systemInfo, &systemId) != XR_SUCCESS) {
+        log_err("Failed to find OpenXR HMD.");
         return false;
     }
 
--- src/xr/XrDisplay.hpp
+++ src/xr/XrDisplay.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+
+#define XR_USE_GRAPHICS_API_VULKAN
 #include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
 
 class XrDisplay
 {
 public:
+    ~XrDisplay();
+
     bool initialize();
-    void cleanup();
+    bool createSession(class Renderer*);
 
     bool shouldQuit = false;
 private:
     bool createInstance();
+    bool findSystem();
     
-    XrInstance instance;
-    XrSession session;
+    XrInstance instance = XR_NULL_HANDLE;
+    XrSystemId systemId = XR_NULL_SYSTEM_ID;
+    XrSession session = XR_NULL_HANDLE;
 };
