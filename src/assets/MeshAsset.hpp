0a9226a328efaa604f1d3c0a54e2d4df91956eb2
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-14 21:48:20 -0600

    Add MeshVertex binding/attribute descriptions

--- src/assets/MeshAsset.hpp
+++ src/assets/MeshAsset.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <string>
 
 #include "api_headers.hpp"
@@ -7,10 +8,42 @@
 class VulkanBuffer;
 class VulkanInstance;
 
+using MeshVertexAttributeDescriptions = std::array<VkVertexInputAttributeDescription, 2>;
+
 struct MeshVertex
 {
     glm::vec3 position;
     glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription description{
+            .binding = 0,
+            .stride = sizeof(MeshVertex),
+            .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+        };
+
+        return description;
+    }
+
+    static MeshVertexAttributeDescriptions getAttributeDescriptions() {
+       MeshVertexAttributeDescriptions descriptions;
+
+       descriptions[0] = {
+           .location = 0,
+           .binding = 0,
+           .format = VK_FORMAT_R32G32B32_SFLOAT,
+           .offset = offsetof(MeshVertex, position)
+       };
+
+       descriptions[1] = {
+           .location = 1,
+           .binding = 0,
+           .format = VK_FORMAT_R32G32B32_SFLOAT,
+           .offset = offsetof(MeshVertex, color)
+       };
+
+       return descriptions;
+    }
 };
 
 using MeshIndex = uint32_t;
