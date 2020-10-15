3e80aba4615854bea1a10ed2c9e2dd2a06ac65d2
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-14 20:41:05 -0600

    Add AssimpLogStream

---
+++ src/log/AssimpLogStream.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "api_headers.hpp"
+
+#include "log/log.hpp"
+
+// I am so sorry to whoever reading this
+// that this class has to exist and that
+// it exists in the way that it does,
+// but Assimp doesn't really give me
+// much of a choice here.
+
+class AssimpLogStream : public Assimp::LogStream
+{
+public:
+    AssimpLogStream(Log::LogLevel logLevel) : logLevel(logLevel) {}
+
+    void write(const char* message) { log_at(logLevel, message); }
+private:
+    Log::LogLevel logLevel;
+};
