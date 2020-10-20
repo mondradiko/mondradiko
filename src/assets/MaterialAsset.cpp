f16753aac217fc0235c9f53c6247c31f31c484da
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-19 18:35:00 -0600

    MaterialAsset loads base color texture

--- src/assets/MaterialAsset.cpp
+++ src/assets/MaterialAsset.cpp
@@ -1,6 +1,16 @@
 #include "assets/MaterialAsset.hpp"
 
-MaterialAsset::MaterialAsset(const aiScene*, aiMaterial*)
+#include "graphics/pipelines/MeshPipeline.hpp"
+
+MaterialAsset::MaterialAsset(MeshPipeline* meshPipeline, std::string fileName, const aiScene* modelScene, aiMaterial* material)
+ : meshPipeline(meshPipeline)
+{
+    aiString baseColor;
+    material->GetTexture(aiTextureType_DIFFUSE, 0, &baseColor);
+    textureBaseColor = meshPipeline->loadTexture(fileName, modelScene, baseColor);
+}
+
+MaterialAsset::~MaterialAsset()
 {
 
 }
--- src/assets/MaterialAsset.hpp
+++ src/assets/MaterialAsset.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <vector>
+
 #include "api_headers.hpp"
 #include "assets/Asset.hpp"
+#include "assets/AssetHandle.hpp"
+#include "assets/TextureAsset.hpp"
+
+class MeshPipeline;
 
 class MaterialAsset : public Asset
 {
 public:
-    MaterialAsset(const aiScene*, aiMaterial*);
+    MaterialAsset(MeshPipeline*, std::string, const aiScene*, aiMaterial*);
+    ~MaterialAsset();
+
+    AssetHandle<TextureAsset> textureBaseColor;
 private:
+    MeshPipeline* meshPipeline;
 };
