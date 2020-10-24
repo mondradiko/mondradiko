8653ce33b64eacb53f768edd2894bb013efd8cf2
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:51:18 -0600

    Make FrameData Google-compliant

--- src/graphics/FrameData.cc
+++ src/graphics/FrameData.cc
@@ -1,97 +1,118 @@
+/**
+ * @file FrameData.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Temp class to encapsulate pipelined frame data.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "graphics/FrameData.h"
 
 #include "graphics/VulkanInstance.h"
 #include "log/log.h"
 
-FrameData::FrameData(VulkanInstance* vulkanInstance, uint32_t framesInFlightCount)
- : vulkanInstance(vulkanInstance),
-   framesInFlight(framesInFlightCount)
-{
-    log_dbg("Creating frame data.");
+FrameData::FrameData(VulkanInstance* vulkanInstance,
+                     uint32_t framesInFlightCount)
+    : vulkanInstance(vulkanInstance), framesInFlight(framesInFlightCount) {
+  log_dbg("Creating frame data.");
 
-    for(FrameInFlight& frame : framesInFlight) {
-        VkCommandBufferAllocateInfo allocInfo{
-            .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-            .commandPool = vulkanInstance->commandPool,
-            .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
-            .commandBufferCount = 1
-        };
+  for (FrameInFlight& frame : framesInFlight) {
+    VkCommandBufferAllocateInfo allocInfo{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .commandPool = vulkanInstance->commandPool,
+        .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        .commandBufferCount = 1};
 
-        if(vkAllocateCommandBuffers(vulkanInstance->device, &allocInfo, &frame.commandBuffer) != VK_SUCCESS) {
-            log_ftl("Failed to allocate frame command buffers.");
-        }
-
-        VkFenceCreateInfo fenceInfo{
-            .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO
-        };
-
-        if(vkCreateFence(vulkanInstance->device, &fenceInfo, nullptr, &frame.isInUse) != VK_SUCCESS) {
-            log_ftl("Failed to create frame fence.");
-        }
-
-        frame.submitted = false;
+    if (vkAllocateCommandBuffers(vulkanInstance->device, &allocInfo,
+                                 &frame.commandBuffer) != VK_SUCCESS) {
+      log_ftl("Failed to allocate frame command buffers.");
     }
+
+    VkFenceCreateInfo fenceInfo{.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO};
+
+    if (vkCreateFence(vulkanInstance->device, &fenceInfo, nullptr,
+                      &frame.isInUse) != VK_SUCCESS) {
+      log_ftl("Failed to create frame fence.");
+    }
+
+    frame.submitted = false;
+  }
 }
 
-FrameData::~FrameData()
-{
-    log_dbg("Destroying frame data.");
+FrameData::~FrameData() {
+  log_dbg("Destroying frame data.");
 
-    for(FrameInFlight& frame : framesInFlight) {
-        vkFreeCommandBuffers(vulkanInstance->device, vulkanInstance->commandPool, 1, &frame.commandBuffer);
-        vkDestroyFence(vulkanInstance->device, frame.isInUse, nullptr);
-    }
+  for (FrameInFlight& frame : framesInFlight) {
+    vkFreeCommandBuffers(vulkanInstance->device, vulkanInstance->commandPool, 1,
+                         &frame.commandBuffer);
+    vkDestroyFence(vulkanInstance->device, frame.isInUse, nullptr);
+  }
 }
 
-VkCommandBuffer FrameData::beginPrimaryCommand()
-{
-    if(framesInFlight[currentFrame].submitted) {
-        vkWaitForFences(vulkanInstance->device, 1, &framesInFlight[currentFrame].isInUse, VK_TRUE, UINT64_MAX);
-        framesInFlight[currentFrame].submitted = false;
-    }
+VkCommandBuffer FrameData::beginPrimaryCommand() {
+  if (framesInFlight[currentFrame].submitted) {
+    vkWaitForFences(vulkanInstance->device, 1,
+                    &framesInFlight[currentFrame].isInUse, VK_TRUE, UINT64_MAX);
+    framesInFlight[currentFrame].submitted = false;
+  }
 
-    if(++currentFrame >= framesInFlight.size()) {
-        currentFrame = 0;
-    }
+  if (++currentFrame >= framesInFlight.size()) {
+    currentFrame = 0;
+  }
 
-    VkCommandBuffer commandBuffer = framesInFlight[currentFrame].commandBuffer;
+  VkCommandBuffer commandBuffer = framesInFlight[currentFrame].commandBuffer;
 
-    VkCommandBufferBeginInfo beginInfo{
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-        .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT
-    };
+  VkCommandBufferBeginInfo beginInfo{
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+      .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT};
 
-    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+  vkBeginCommandBuffer(commandBuffer, &beginInfo);
 
-    return commandBuffer;
+  return commandBuffer;
 }
 
-void FrameData::endPrimaryCommand()
-{
-    vkEndCommandBuffer(framesInFlight[currentFrame].commandBuffer);
+void FrameData::endPrimaryCommand() {
+  vkEndCommandBuffer(framesInFlight[currentFrame].commandBuffer);
 }
 
-void FrameData::submitPrimaryCommand()
-{
-    VkSubmitInfo submitInfo{};
-    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+void FrameData::submitPrimaryCommand() {
+  VkSubmitInfo submitInfo{};
+  submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 
-    VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-    submitInfo.waitSemaphoreCount = 0;
-    submitInfo.pWaitSemaphores = nullptr;
-    submitInfo.pWaitDstStageMask = waitStages;
+  VkPipelineStageFlags waitStages[] = {
+      VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+  submitInfo.waitSemaphoreCount = 0;
+  submitInfo.pWaitSemaphores = nullptr;
+  submitInfo.pWaitDstStageMask = waitStages;
 
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &framesInFlight[currentFrame].commandBuffer;
+  submitInfo.commandBufferCount = 1;
+  submitInfo.pCommandBuffers = &framesInFlight[currentFrame].commandBuffer;
 
-    submitInfo.signalSemaphoreCount = 0;
-    submitInfo.pSignalSemaphores = nullptr;
+  submitInfo.signalSemaphoreCount = 0;
+  submitInfo.pSignalSemaphores = nullptr;
 
-    vkResetFences(vulkanInstance->device, 1, &framesInFlight[currentFrame].isInUse);
+  vkResetFences(vulkanInstance->device, 1,
+                &framesInFlight[currentFrame].isInUse);
 
-    if(vkQueueSubmit(vulkanInstance->graphicsQueue, 1, &submitInfo, framesInFlight[currentFrame].isInUse) != VK_SUCCESS) {
-        log_ftl("Failed to submit primary frame command buffer.");
-    }
+  if (vkQueueSubmit(vulkanInstance->graphicsQueue, 1, &submitInfo,
+                    framesInFlight[currentFrame].isInUse) != VK_SUCCESS) {
+    log_ftl("Failed to submit primary frame command buffer.");
+  }
 
-    framesInFlight[currentFrame].submitted = true;
+  framesInFlight[currentFrame].submitted = true;
 }
--- src/graphics/FrameData.h
+++ src/graphics/FrameData.h
@@ -1,30 +1,54 @@
-#pragma once
+/**
+ * @file FrameData.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Temp class to encapsulate pipelined frame data.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_GRAPHICS_FRAMEDATA_H_
+#define SRC_GRAPHICS_FRAMEDATA_H_
 
 #include <vector>
 
-#include "api_headers.h"
+#include "graphics/VulkanInstance.h"
+#include "src/api_headers.h"
 
-class VulkanInstance;
-
-struct FrameInFlight
-{
-    VkCommandBuffer commandBuffer;
-    VkFence isInUse;
-    bool submitted;
+struct FrameInFlight {
+  VkCommandBuffer commandBuffer;
+  VkFence isInUse;
+  bool submitted;
 };
 
-class FrameData
-{
-public:
-    FrameData(VulkanInstance*, uint32_t);
-    ~FrameData();
+class FrameData {
+ public:
+  FrameData(VulkanInstance*, uint32_t);
+  ~FrameData();
 
-    VkCommandBuffer beginPrimaryCommand();
-    void endPrimaryCommand();
-    void submitPrimaryCommand();
-private:
-    VulkanInstance* vulkanInstance;
+  VkCommandBuffer beginPrimaryCommand();
+  void endPrimaryCommand();
+  void submitPrimaryCommand();
 
-    uint32_t currentFrame = 0;
-    std::vector<FrameInFlight> framesInFlight;
+ private:
+  VulkanInstance* vulkanInstance;
+
+  uint32_t currentFrame = 0;
+  std::vector<FrameInFlight> framesInFlight;
 };
+
+#endif  // SRC_GRAPHICS_FRAMEDATA_H_
