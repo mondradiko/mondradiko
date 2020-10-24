5b9ff7873c19aeef5cb1908afaaf91645643b87b
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:30:31 -0600

    Make AssimpIOSystem Google-compliant

--- src/filesystem/AssimpIOSystem.cc
+++ src/filesystem/AssimpIOSystem.cc
@@ -1,31 +1,42 @@
+/**
+ * @file AssimpIOSystem.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Inherits Assimp::IOSystem to integrate Assimp into the Filesystem.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "filesystem/AssimpIOSystem.h"
 
 #include "filesystem/AssimpIOStream.h"
 #include "filesystem/Filesystem.h"
 #include "log/log.h"
 
-AssimpIOSystem::AssimpIOSystem(Filesystem* fs)
- : fs(fs)
-{
+AssimpIOSystem::AssimpIOSystem(Filesystem* fs) : fs(fs) {}
 
+AssimpIOSystem::~AssimpIOSystem() {}
+
+bool AssimpIOSystem::Exists(const char* fileName) const {
+  return fs->exists(fileName);
 }
 
-AssimpIOSystem::~AssimpIOSystem()
-{
-
+Assimp::IOStream* AssimpIOSystem::Open(const char* fileName, const char* mode) {
+  return new AssimpIOStream(fs, fileName, mode);
 }
 
-bool AssimpIOSystem::Exists(const char* fileName) const
-{
-    return fs->exists(fileName);
-}
-
-Assimp::IOStream* AssimpIOSystem::Open(const char* fileName, const char* mode)
-{
-    return new AssimpIOStream(fs, fileName, mode);
-}
-
-void AssimpIOSystem::Close(Assimp::IOStream* file)
-{
-
-}
+void AssimpIOSystem::Close(Assimp::IOStream* file) {}
--- src/filesystem/AssimpIOSystem.h
+++ src/filesystem/AssimpIOSystem.h
@@ -1,19 +1,44 @@
-#pragma once
+/**
+ * @file AssimpIOSystem.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Inherits Assimp::IOSystem to integrate Assimp into the Filesystem.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
 
-#include "api_headers.h"
+#ifndef SRC_FILESYSTEM_ASSIMPIOSYSTEM_H_
+#define SRC_FILESYSTEM_ASSIMPIOSYSTEM_H_
 
-class Filesystem;
+#include "filesystem/Filesystem.h"
+#include "src/api_headers.h"
 
-class AssimpIOSystem : public Assimp::IOSystem
-{
-public:
-    AssimpIOSystem(Filesystem*);
-    ~AssimpIOSystem();
+class AssimpIOSystem : public Assimp::IOSystem {
+ public:
+  explicit AssimpIOSystem(Filesystem*);
+  ~AssimpIOSystem();
 
-    bool Exists(const char*) const;
-    char getOsSeparator() const { return '/'; }
-    Assimp::IOStream* Open(const char*, const char*);
-    void Close(Assimp::IOStream*);
-private:
-    Filesystem* fs;
+  bool Exists(const char*) const;
+  char getOsSeparator() const { return '/'; }
+  Assimp::IOStream* Open(const char*, const char*);
+  void Close(Assimp::IOStream*);
+
+ private:
+  Filesystem* fs;
 };
+
+#endif  // SRC_FILESYSTEM_ASSIMPIOSYSTEM_H_
