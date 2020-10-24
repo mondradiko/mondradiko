ea298e6631bb4d32ac39256d7ff5f21dd6e915e7
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:46:02 -0600

    Make ShaderModule Google-compliant

--- src/graphics/shaders/ShaderModule.cc
+++ src/graphics/shaders/ShaderModule.cc
@@ -1,35 +1,56 @@
+/**
+ * @file ShaderModule.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Base class to compile shader code and provide creation info.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "graphics/shaders/ShaderModule.h"
 
 #include "graphics/VulkanInstance.h"
 #include "log/log.h"
 
-ShaderModule::ShaderModule(VulkanInstance* _vki, std::string _shaderName, shaderc_shader_kind _shaderKind)
-{
-    vki = _vki;
-    shaderName = _shaderName;
-    shaderKind = _shaderKind;
+ShaderModule::ShaderModule(VulkanInstance* _vki, std::string _shaderName,
+                           shaderc_shader_kind _shaderKind) {
+  vki = _vki;
+  shaderName = _shaderName;
+  shaderKind = _shaderKind;
 
-    compiled = false;
+  compiled = false;
 
-    glslSource << "#version 450" << std::endl;
-    glslSource << "#extension GL_ARB_separate_shader_objects : enable" << std::endl;
+  glslSource << "#version 450" << std::endl;
+  glslSource << "#extension GL_ARB_separate_shader_objects : enable"
+             << std::endl;
 }
 
-ShaderModule::~ShaderModule()
-{
-    if(compiled) {
-        vkDestroyShaderModule(vki->device, shaderModule, nullptr);
-    }
+ShaderModule::~ShaderModule() {
+  if (compiled) {
+    vkDestroyShaderModule(vki->device, shaderModule, nullptr);
+  }
 }
 
-void ShaderModule::pushCustom(std::string code)
-{
-    glslSource << code << std::endl;
+void ShaderModule::pushCustom(std::string code) {
+  glslSource << code << std::endl;
 }
 
-void ShaderModule::pushFullscreenQuad()
-{
-    glslSource << R"""(
+void ShaderModule::pushFullscreenQuad() {
+  glslSource << R"""(
 layout(location = 0) out vec2 fragTexCoord;
 
 vec2 positions[4] = vec2[](
@@ -53,58 +74,54 @@ void main() {
     )""" << std::endl;
 }
 
-VkShaderModule ShaderModule::compile()
-{
-    if(compiled) return shaderModule;
+VkShaderModule ShaderModule::compile() {
+  if (compiled) return shaderModule;
 
-    shaderc::Compiler compiler;
-    shaderc::CompileOptions options;
+  shaderc::Compiler compiler;
+  shaderc::CompileOptions options;
 
-    std::string completeSource = glslSource.str();
-    auto spirvModule = compiler.CompileGlslToSpv(completeSource, shaderKind, shaderName.data(), options);
+  std::string completeSource = glslSource.str();
+  auto spirvModule = compiler.CompileGlslToSpv(completeSource, shaderKind,
+                                               shaderName.data(), options);
 
-    if(spirvModule.GetCompilationStatus() != shaderc_compilation_status_success) {
-        std::ostringstream errorMessage;
-        errorMessage << "Shader " << shaderName;
-        errorMessage << " compilation failed with ";
-        errorMessage << spirvModule.GetErrorMessage();
-        log_ftl(errorMessage.str().c_str());
-    }
+  if (spirvModule.GetCompilationStatus() !=
+      shaderc_compilation_status_success) {
+    std::ostringstream errorMessage;
+    errorMessage << "Shader " << shaderName;
+    errorMessage << " compilation failed with ";
+    errorMessage << spirvModule.GetErrorMessage();
+    log_ftl(errorMessage.str().c_str());
+  }
 
-    std::vector<uint32_t> spirv = {spirvModule.cbegin(), spirvModule.cend()};
+  std::vector<uint32_t> spirv = {spirvModule.cbegin(), spirvModule.cend()};
 
-    VkShaderModuleCreateInfo createInfo{
-        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
-        .codeSize = spirv.size() * sizeof(uint32_t),
-        .pCode = spirv.data()
-    };
+  VkShaderModuleCreateInfo createInfo{
+      .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+      .codeSize = spirv.size() * sizeof(uint32_t),
+      .pCode = spirv.data()};
 
-    if(vkCreateShaderModule(vki->device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
-        log_ftl("Failed to create shader module");
-    }
+  if (vkCreateShaderModule(vki->device, &createInfo, nullptr, &shaderModule) !=
+      VK_SUCCESS) {
+    log_ftl("Failed to create shader module");
+  }
 
-    compiled = true;
-    return shaderModule;
+  compiled = true;
+  return shaderModule;
 }
 
-VkPipelineShaderStageCreateInfo ShaderModule::getStageCreateInfo()
-{
-    if(shaderKind == shaderc_vertex_shader) {
-        return{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+VkPipelineShaderStageCreateInfo ShaderModule::getStageCreateInfo() {
+  if (shaderKind == shaderc_vertex_shader) {
+    return {.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
             .stage = VK_SHADER_STAGE_VERTEX_BIT,
             .module = shaderModule,
-            .pName = "main"
-        };
-    } else if(shaderKind == shaderc_fragment_shader) {
-        return{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+            .pName = "main"};
+  } else if (shaderKind == shaderc_fragment_shader) {
+    return {.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
             .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
             .module = shaderModule,
-            .pName = "main"
-        };
-    } else {
-        log_ftl("Unrecognized shader type");
-        return {};
-    }
+            .pName = "main"};
+  } else {
+    log_ftl("Unrecognized shader type");
+    return {};
+  }
 }
--- src/graphics/shaders/ShaderModule.h
+++ src/graphics/shaders/ShaderModule.h
@@ -1,30 +1,56 @@
-#pragma once
+/**
+ * @file ShaderModule.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Base class to compile shader code and provide creation info.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_GRAPHICS_SHADERS_SHADERMODULE_H_
+#define SRC_GRAPHICS_SHADERS_SHADERMODULE_H_
 
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "api_headers.h"
+#include "graphics/VulkanInstance.h"
+#include "src/api_headers.h"
 
-class VulkanInstance;
 using ShaderStages = std::vector<VkPipelineShaderStageCreateInfo>;
 
-class ShaderModule
-{
-public:
-    ShaderModule(VulkanInstance*, std::string, shaderc_shader_kind);
-    ~ShaderModule();
+class ShaderModule {
+ public:
+  ShaderModule(VulkanInstance*, std::string, shaderc_shader_kind);
+  ~ShaderModule();
 
-    void pushCustom(std::string);
-    void pushFullscreenQuad();
+  void pushCustom(std::string);
+  void pushFullscreenQuad();
 
-    VkShaderModule compile();
-    VkPipelineShaderStageCreateInfo getStageCreateInfo();
-private:
-    VulkanInstance* vki;
-    std::ostringstream glslSource;
-    std::string shaderName;
-    shaderc_shader_kind shaderKind;
-    VkShaderModule shaderModule;
-    bool compiled;
+  VkShaderModule compile();
+  VkPipelineShaderStageCreateInfo getStageCreateInfo();
+
+ private:
+  VulkanInstance* vki;
+  std::ostringstream glslSource;
+  std::string shaderName;
+  shaderc_shader_kind shaderKind;
+  VkShaderModule shaderModule;
+  bool compiled;
 };
+
+#endif  // SRC_GRAPHICS_SHADERS_SHADERMODULE_H_
