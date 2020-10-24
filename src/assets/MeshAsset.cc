14fb56c89d8df97326959b440d9787fe946ee95d
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:07:43 -0600

    Make MeshAsset Google-compliant

--- src/assets/MeshAsset.cc
+++ src/assets/MeshAsset.cc
@@ -1,75 +1,106 @@
+/**
+ * @file MeshAsset.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains the geometry for a mesh.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "assets/MeshAsset.h"
+
+#include <vector>
 
 #include "graphics/VulkanBuffer.h"
 #include "graphics/VulkanInstance.h"
 #include "log/log.h"
 
-MeshAsset::MeshAsset(std::string meshName, VulkanInstance* vulkanInstance, aiMesh* mesh)
- : meshName(meshName),
-   vulkanInstance(vulkanInstance)
-{
-    log_dbg("Loading mesh asset with name:");
-    log_dbg(meshName.c_str());
+MeshAsset::MeshAsset(std::string meshName, VulkanInstance* vulkanInstance,
+                     aiMesh* mesh)
+    : meshName(meshName), vulkanInstance(vulkanInstance) {
+  log_dbg("Loading mesh asset with name:");
+  log_dbg(meshName.c_str());
 
-    if((mesh->mPrimitiveTypes & aiPrimitiveType_TRIANGLE) != aiPrimitiveType_TRIANGLE) {
-        log_ftl("Mesh topology is not exclusively triangles.");
+  if ((mesh->mPrimitiveTypes & aiPrimitiveType_TRIANGLE) !=
+      aiPrimitiveType_TRIANGLE) {
+    log_ftl("Mesh topology is not exclusively triangles.");
+  }
+
+  if (!mesh->HasPositions()) {
+    log_ftl("Mesh vertices have no positions.");
+  }
+
+  if (!mesh->HasNormals()) {
+    log_ftl("Mesh vertices have no normals.");
+  }
+
+  if (!mesh->HasTextureCoords(0)) {
+    log_ftl("Mesh vertices have no texture coordinates.");
+  }
+
+  std::vector<MeshVertex> vertices(mesh->mNumVertices);
+
+  for (uint32_t vertexIndex = 0; vertexIndex < mesh->mNumVertices;
+       vertexIndex++) {
+    aiVector3D position = mesh->mVertices[vertexIndex];
+    aiVector3D normal = mesh->mNormals[vertexIndex];
+    aiVector3D texCoord = mesh->mTextureCoords[0][vertexIndex];
+
+    vertices[vertexIndex].position =
+        glm::vec3(position.x, position.y, position.z);
+    vertices[vertexIndex].normal = glm::vec3(normal.x, normal.y, normal.z);
+    // Take only the UV coordinates
+    // Invert the Y coordinate into range [0, 1]
+    vertices[vertexIndex].texCoord = glm::vec2(texCoord.x, -texCoord.y);
+  }
+
+  // Three indices per triangle face
+  indexCount = mesh->mNumFaces * 3;
+  std::vector<MeshIndex> indices(indexCount);
+
+  // lol
+  uint32_t indexIndex = 0;
+  for (uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces; faceIndex++) {
+    aiFace& face = mesh->mFaces[faceIndex];
+
+    for (uint32_t faceCornerIndex = 0; faceCornerIndex < face.mNumIndices;
+         faceCornerIndex++) {
+      indices[indexIndex] = face.mIndices[faceCornerIndex];
+      indexIndex++;
     }
+  }
 
-    if(!mesh->HasPositions()) {
-        log_ftl("Mesh vertices have no positions.");
-    }
+  size_t vertexSize = sizeof(MeshVertex) * vertices.size();
+  vertexBuffer = new VulkanBuffer(vulkanInstance, vertexSize,
+                                  VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                                  VMA_MEMORY_USAGE_CPU_TO_GPU);
+  vertexBuffer->writeData(vertices.data());
 
-    if(!mesh->HasNormals()) {
-        log_ftl("Mesh vertices have no normals.");
-    }
-
-    if(!mesh->HasTextureCoords(0)) {
-        log_ftl("Mesh vertices have no texture coordinates.");
-    }
-
-    std::vector<MeshVertex> vertices(mesh->mNumVertices);
-
-    for(uint32_t vertexIndex = 0; vertexIndex < mesh->mNumVertices; vertexIndex++) {
-        aiVector3D position = mesh->mVertices[vertexIndex];
-        aiVector3D normal = mesh->mNormals[vertexIndex];
-        aiVector3D texCoord = mesh->mTextureCoords[0][vertexIndex];
-
-        vertices[vertexIndex].position = glm::vec3(position.x, position.y, position.z);
-        vertices[vertexIndex].normal = glm::vec3(normal.x, normal.y, normal.z);
-        // Take only the UV coordinates
-        // Invert the Y coordinate into range [0, 1]
-        vertices[vertexIndex].texCoord = glm::vec2(texCoord.x, -texCoord.y);
-    }
-
-    // Three indices per triangle face
-    indexCount = mesh->mNumFaces * 3;
-    std::vector<MeshIndex> indices(indexCount);
-
-    // lol
-    uint32_t indexIndex = 0;
-    for(uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces; faceIndex++) {
-        aiFace& face = mesh->mFaces[faceIndex];
-
-        for(uint32_t faceCornerIndex = 0; faceCornerIndex < face.mNumIndices; faceCornerIndex++) {
-            indices[indexIndex] = face.mIndices[faceCornerIndex];
-            indexIndex++;
-        }
-    }
-
-    size_t vertexSize = sizeof(MeshVertex) * vertices.size();
-    vertexBuffer = new VulkanBuffer(vulkanInstance, vertexSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
-    vertexBuffer->writeData(vertices.data());
-
-    size_t indexSize = sizeof(indices[0]) * indices.size();
-    indexBuffer = new VulkanBuffer(vulkanInstance, indexSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
-    indexBuffer->writeData(indices.data());
+  size_t indexSize = sizeof(indices[0]) * indices.size();
+  indexBuffer = new VulkanBuffer(vulkanInstance, indexSize,
+                                 VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+                                 VMA_MEMORY_USAGE_CPU_TO_GPU);
+  indexBuffer->writeData(indices.data());
 }
 
-MeshAsset::~MeshAsset()
-{
-    log_dbg("Destroying mesh asset:");
-    log_dbg(meshName.c_str());
-    
-    if(vertexBuffer != nullptr) delete vertexBuffer;
-    if(indexBuffer != nullptr) delete indexBuffer;
+MeshAsset::~MeshAsset() {
+  log_dbg("Destroying mesh asset:");
+  log_dbg(meshName.c_str());
+
+  if (vertexBuffer != nullptr) delete vertexBuffer;
+  if (indexBuffer != nullptr) delete indexBuffer;
 }
--- src/assets/MeshAsset.h
+++ src/assets/MeshAsset.h
@@ -1,73 +1,91 @@
-#pragma once
+/**
+ * @file MeshAsset.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains the geometry for a mesh.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_ASSETS_MESHASSET_H_
+#define SRC_ASSETS_MESHASSET_H_
 
 #include <array>
 #include <string>
 
-#include "api_headers.h"
+#include "src/api_headers.h"
 #include "assets/Asset.h"
+#include "graphics/VulkanBuffer.h"
+#include "graphics/VulkanInstance.h"
 
-class VulkanBuffer;
-class VulkanInstance;
+using MeshVertexAttributeDescriptions =
+    std::array<VkVertexInputAttributeDescription, 3>;
 
-using MeshVertexAttributeDescriptions = std::array<VkVertexInputAttributeDescription, 3>;
+struct MeshVertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec2 texCoord;
 
-struct MeshVertex
-{
-    glm::vec3 position;
-    glm::vec3 normal;
-    glm::vec2 texCoord;
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription description{
+        .binding = 0,
+        .stride = sizeof(MeshVertex),
+        .inputRate = VK_VERTEX_INPUT_RATE_VERTEX};
 
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription description{
-            .binding = 0,
-            .stride = sizeof(MeshVertex),
-            .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
-        };
+    return description;
+  }
 
-        return description;
-    }
+  static MeshVertexAttributeDescriptions getAttributeDescriptions() {
+    MeshVertexAttributeDescriptions descriptions;
 
-    static MeshVertexAttributeDescriptions getAttributeDescriptions() {
-       MeshVertexAttributeDescriptions descriptions;
+    descriptions[0] = {.location = 0,
+                       .binding = 0,
+                       .format = VK_FORMAT_R32G32B32_SFLOAT,
+                       .offset = offsetof(MeshVertex, position)};
 
-       descriptions[0] = {
-           .location = 0,
-           .binding = 0,
-           .format = VK_FORMAT_R32G32B32_SFLOAT,
-           .offset = offsetof(MeshVertex, position)
-       };
+    descriptions[1] = {.location = 1,
+                       .binding = 0,
+                       .format = VK_FORMAT_R32G32B32_SFLOAT,
+                       .offset = offsetof(MeshVertex, normal)};
 
-       descriptions[1] = {
-           .location = 1,
-           .binding = 0,
-           .format = VK_FORMAT_R32G32B32_SFLOAT,
-           .offset = offsetof(MeshVertex, normal)
-       };
+    descriptions[2] = {.location = 2,
+                       .binding = 0,
+                       .format = VK_FORMAT_R32G32_SFLOAT,
+                       .offset = offsetof(MeshVertex, texCoord)};
 
-       descriptions[2] = {
-           .location = 2,
-           .binding = 0,
-           .format = VK_FORMAT_R32G32_SFLOAT,
-           .offset = offsetof(MeshVertex, texCoord)
-       };
-
-       return descriptions;
-    }
+    return descriptions;
+  }
 };
 
 using MeshIndex = uint32_t;
 
-class MeshAsset : public Asset
-{
-public:
-    MeshAsset(std::string, VulkanInstance*, aiMesh*);
-    ~MeshAsset();
+class MeshAsset : public Asset {
+ public:
+  MeshAsset(std::string, VulkanInstance*, aiMesh*);
+  ~MeshAsset();
 
-    VulkanBuffer* vertexBuffer = nullptr;
-    VulkanBuffer* indexBuffer = nullptr;
-    size_t indexCount = 0;
+  VulkanBuffer* vertexBuffer = nullptr;
+  VulkanBuffer* indexBuffer = nullptr;
+  size_t indexCount = 0;
 
-    std::string meshName = nullptr;
-private:
-    VulkanInstance* vulkanInstance;
+  std::string meshName = nullptr;
+
+ private:
+  VulkanInstance* vulkanInstance;
 };
+
+#endif  // SRC_ASSETS_MESHASSET_H_
