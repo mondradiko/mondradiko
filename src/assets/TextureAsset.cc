9d6ce4d88c4df66daf8cf6ced4101f78c3b65714
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:11:00 -0600

    Make TextureAsset Google-compliant

--- src/assets/TextureAsset.cc
+++ src/assets/TextureAsset.cc
@@ -1,20 +1,40 @@
+/**
+ * @file TextureAsset.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains a texture, intended for mapping to meshes.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "assets/TextureAsset.h"
 
 #include "graphics/VulkanImage.h"
 #include "log/log.h"
 
 TextureAsset::TextureAsset(VulkanInstance* vulkanInstance, aiTexture* texture)
- : vulkanInstance(vulkanInstance)
-{
-    image = new VulkanImage(vulkanInstance,
-        VK_FORMAT_R8G8B8A8_UNORM,
-        texture->mWidth, texture->mHeight,
-        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-        VMA_MEMORY_USAGE_GPU_ONLY
-    );
+    : vulkanInstance(vulkanInstance) {
+  image = new VulkanImage(
+      vulkanInstance, VK_FORMAT_R8G8B8A8_UNORM, texture->mWidth,
+      texture->mHeight,
+      VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+      VMA_MEMORY_USAGE_GPU_ONLY);
 }
 
-TextureAsset::~TextureAsset()
-{
-    if(image) delete image;
+TextureAsset::~TextureAsset() {
+  if (image) delete image;
 }
--- src/assets/TextureAsset.h
+++ src/assets/TextureAsset.h
@@ -1,20 +1,46 @@
-#pragma once
+/**
+ * @file TextureAsset.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains a texture, intended for mapping to meshes.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_ASSETS_TEXTUREASSET_H_
+#define SRC_ASSETS_TEXTUREASSET_H_
 
 #include <string>
 
-#include "api_headers.h"
 #include "assets/Asset.h"
+#include "src/api_headers.h"
 
 class VulkanImage;
 class VulkanInstance;
 
-class TextureAsset : public Asset
-{
-public:
-    TextureAsset(VulkanInstance*, aiTexture*);
-    ~TextureAsset();
+class TextureAsset : public Asset {
+ public:
+  TextureAsset(VulkanInstance*, aiTexture*);
+  ~TextureAsset();
 
-    VulkanImage* image = nullptr;
-private:
-    VulkanInstance* vulkanInstance;
+  VulkanImage* image = nullptr;
+
+ private:
+  VulkanInstance* vulkanInstance;
 };
+
+#endif  // SRC_ASSETS_TEXTUREASSET_H_
