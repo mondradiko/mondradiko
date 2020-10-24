900c7680f0f441febe34ceaf0a73a6d0ac17826d
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:28:43 -0600

    Make AssimpIOStream Google-compliant

--- src/filesystem/AssimpIOStream.cc
+++ src/filesystem/AssimpIOStream.cc
@@ -1,54 +1,71 @@
+/**
+ * @file AssimpIOStream.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Inherits the Assimp::IOStream class to provide logging functionality.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "filesystem/AssimpIOStream.h"
 
 #include "filesystem/Filesystem.h"
 
-AssimpIOStream::AssimpIOStream(Filesystem* fs, const char* fileName, const char* mode)
- : fs(fs)
-{
-    fs->loadBinaryFile(fileName, &contents);
-    position = 0;
+AssimpIOStream::AssimpIOStream(Filesystem* fs, const char* fileName,
+                               const char* mode)
+    : fs(fs) {
+  fs->loadBinaryFile(fileName, &contents);
+  position = 0;
 }
 
-AssimpIOStream::~AssimpIOStream()
-{
+AssimpIOStream::~AssimpIOStream() {}
 
+size_t AssimpIOStream::Read(void* data, size_t size, size_t count) {
+  size_t readNum = size * count;
+  // TODO(marceline-cramer) This isn't safe
+  memcpy(data, contents.data() + position, readNum);
+  return readNum;
 }
 
-size_t AssimpIOStream::Read(void* data, size_t size, size_t count)
-{
-    size_t readNum = size * count;
-    // TODO This isn't safe
-    memcpy(data, contents.data() + position, readNum);
-    return readNum;
+size_t AssimpIOStream::Write(const void*, size_t, size_t) {
+  // Always fails because no writing is allowed
+  return 0;
 }
 
-size_t AssimpIOStream::Write(const void*, size_t, size_t)
-{
-    // Always fails because no writing is allowed
-    return 0;
+aiReturn AssimpIOStream::Seek(size_t offset, aiOrigin origin) {
+  switch (origin) {
+    case aiOrigin_SET:
+      position = offset;
+      return aiReturn_SUCCESS;
+    case aiOrigin_CUR:
+      position += offset;
+      return aiReturn_SUCCESS;
+    case aiOrigin_END:
+      position = FileSize() + offset;
+      return aiReturn_SUCCESS;
+    default:
+      return aiReturn_FAILURE;
+  }
 }
 
-aiReturn AssimpIOStream::Seek(size_t offset, aiOrigin origin)
-{
-    switch(origin) {
-        case aiOrigin_SET: position = offset; return aiReturn_SUCCESS;
-        case aiOrigin_CUR: position += offset; return aiReturn_SUCCESS;
-        case aiOrigin_END: position = FileSize() + offset; return aiReturn_SUCCESS;
-        default: return aiReturn_FAILURE;
-    }
-}
+size_t AssimpIOStream::Tell() const { return position; }
 
-size_t AssimpIOStream::Tell() const
-{
-    return position;
-}
+size_t AssimpIOStream::FileSize() const { return contents.size(); }
 
-size_t AssimpIOStream::FileSize() const
-{
-    return contents.size();
-}
-
-void AssimpIOStream::Flush()
-{
-    // We're not allowing writing, so flushing is not necessary
+void AssimpIOStream::Flush() {
+  // We're not allowing writing, so flushing is not necessary
 }
--- src/filesystem/AssimpIOStream.h
+++ src/filesystem/AssimpIOStream.h
@@ -1,26 +1,51 @@
-#pragma once
+/**
+ * @file AssimpIOStream.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Inherits the Assimp::IOStream class to provide logging functionality.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_FILESYSTEM_ASSIMPIOSTREAM_H_
+#define SRC_FILESYSTEM_ASSIMPIOSTREAM_H_
 
 #include <vector>
 
-#include "api_headers.h"
+#include "filesystem/Filesystem.h"
+#include "src/api_headers.h"
 
-class Filesystem;
+class AssimpIOStream : public Assimp::IOStream {
+ public:
+  AssimpIOStream(Filesystem*, const char*, const char*);
+  ~AssimpIOStream();
 
-class AssimpIOStream : public Assimp::IOStream
-{
-public:
-    AssimpIOStream(Filesystem*, const char*, const char*);
-    ~AssimpIOStream();
+  size_t Read(void*, size_t, size_t);
+  size_t Write(const void*, size_t, size_t);
+  aiReturn Seek(size_t, aiOrigin);
+  size_t Tell() const;
+  size_t FileSize() const;
+  void Flush();
 
-    size_t Read(void*, size_t, size_t);
-    size_t Write(const void*, size_t, size_t);
-    aiReturn Seek(size_t, aiOrigin);
-    size_t Tell() const;
-    size_t FileSize() const;
-    void Flush();
-private:
-    Filesystem* fs;
+ private:
+  Filesystem* fs;
 
-    std::vector<char> contents;
-    size_t position;
+  std::vector<char> contents;
+  size_t position;
 };
+
+#endif  // SRC_FILESYSTEM_ASSIMPIOSTREAM_H_
