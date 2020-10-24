00c1a1dd28669098fa9c1fba6a762293c0f8ebbb
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 15:59:13 -0600

    Make client_session Google-compliant

--- src/session/client_session.cc
+++ src/session/client_session.cc
@@ -1,3 +1,24 @@
+/**
+ * @file client_session.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Runs the lifetime of a single Mondradiko client session.
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include "session/client_session.h"
 
 #include <csignal>
@@ -16,54 +37,54 @@
 bool shouldQuit = false;
 
 void signalHandler(int signum) {
-   std::cout << "Interrupt signal (" << signum << ") received.\n";
-   shouldQuit = true;
+  std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+  shouldQuit = true;
 
-   return;
+  return;
 }
 
-void client_session_run(const char* serverAddress, int port)
-{
-    Filesystem fs("../test-folder/");
-    XrDisplay xr;
-    VulkanInstance vulkanInstance(&xr);
-    PlayerSession session(&xr, &vulkanInstance);
-    Renderer renderer(&vulkanInstance, &session);
-    ClientInterface client(serverAddress, port);
-    Scene scene(&fs, &renderer);
+void client_session_run(const char *serverAddress, int port) {
+  Filesystem fs("../test-folder/");
+  XrDisplay xr;
+  VulkanInstance vulkanInstance(&xr);
+  PlayerSession session(&xr, &vulkanInstance);
+  Renderer renderer(&vulkanInstance, &session);
+  ClientInterface client(serverAddress, port);
+  Scene scene(&fs, &renderer);
 
-    if(signal(SIGTERM, signalHandler) == SIG_ERR) {
-        log_wrn("Can't catch SIGTERM");
+  if (signal(SIGTERM, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGTERM");
+  }
+
+  if (signal(SIGINT, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGINT");
+  }
+
+  bool shouldRun = false;
+
+  while (true) {
+    session.pollEvents(&shouldRun, &shouldQuit);
+    if (shouldQuit)
+      break;
+
+    if (shouldRun) {
+      double dt;
+      bool shouldRender;
+      session.beginFrame(&dt, &shouldRender);
+
+      client.update();
+      scene.update(dt);
+
+      ClientEvent event;
+      while (client.readEvent(&event)) {
+        log_dbg("Received client event.");
+      }
+
+      if (shouldRender) {
+        renderer.renderFrame();
+      }
+
+      session.endFrame(&renderer, shouldRender);
     }
-
-    if(signal(SIGINT, signalHandler) == SIG_ERR) {
-        log_wrn("Can't catch SIGINT");
-    }
-
-    bool shouldRun = false;
-
-    while(true) {
-        session.pollEvents(&shouldRun, &shouldQuit);
-        if(shouldQuit) break;
-
-        if(shouldRun) {
-            double dt;
-            bool shouldRender;
-            session.beginFrame(&dt, &shouldRender);
-
-            client.update();
-            scene.update(dt);
-
-            ClientEvent event;
-            while(client.readEvent(&event)) {
-                log_dbg("Received client event.");
-            }
-
-            if(shouldRender) {
-                renderer.renderFrame();
-            }
-
-            session.endFrame(&renderer, shouldRender);
-        }
-    }
+  }
 }
--- src/session/client_session.h
+++ src/session/client_session.h
@@ -1,3 +1,27 @@
-#pragma once
+/**
+ * @file client_session.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Runs the lifetime of a single Mondradiko client session.
+ * @copyright Copyright (c) 2020
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef SRC_SESSION_CLIENT_SESSION_H_
+#define SRC_SESSION_CLIENT_SESSION_H_
 
 void client_session_run(const char* serverAddress, int serverPort);
+
+#endif  // SRC_SESSION_CLIENT_SESSION_H_
