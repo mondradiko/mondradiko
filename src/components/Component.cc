609c113498c9f7d29df3e992093827a3ce0046ff
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:21:57 -0600

    Make Component Google-compliant

--- src/components/Component.cc
+++ src/components/Component.cc
@@ -1,10 +1,32 @@
+/**
+ * @file Component.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Implements reference counting of Assets.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "components/Component.h"
 
 #include "scene/Entity.h"
 
-Component::~Component()
-{
-    parent->firstComponent = next;
-    if(prev) prev->next = next;
-    if(next) next->prev = prev;
+Component::~Component() {
+  parent->firstComponent = next;
+  if (prev) prev->next = next;
+  if (next) next->prev = prev;
 }
--- src/components/Component.h
+++ src/components/Component.h
@@ -1,15 +1,41 @@
-#pragma once
+/**
+ * @file Component.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Implements reference counting of Assets.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
 
-class Entity;
+#ifndef SRC_COMPONENTS_COMPONENT_H_
+#define SRC_COMPONENTS_COMPONENT_H_
 
-class Component
-{
-public:
-    virtual ~Component();
-private:
-    friend class Entity;
+class Entity; // Forward declaration because of codependence
 
-    Entity* parent = nullptr;
-    Component* prev = nullptr;
-    Component* next = nullptr;
+class Component {
+ public:
+  virtual ~Component();
+
+ private:
+  friend class Entity;
+
+  Entity* parent = nullptr;
+  Component* prev = nullptr;
+  Component* next = nullptr;
 };
+
+#endif  // SRC_COMPONENTS_COMPONENT_H_
