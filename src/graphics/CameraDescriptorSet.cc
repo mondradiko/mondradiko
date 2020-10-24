0710027d86fb4d65f9ce1496847095c305c45582
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 17:48:56 -0600

    Make CameraDescriptorSet Google-compliant

--- src/graphics/CameraDescriptorSet.cc
+++ src/graphics/CameraDescriptorSet.cc
@@ -1,131 +1,146 @@
+/**
+ * @file CameraDescriptorSet.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains uniform data for each view.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "graphics/CameraDescriptorSet.h"
 
 #include "graphics/VulkanBuffer.h"
 #include "graphics/VulkanInstance.h"
 #include "log/log.h"
 
-static glm::mat4 createProjectionFromFOV(const XrFovf fov, const float near_z, const float far_z)
-{
-    const float tan_left = tanf(fov.angleLeft);
-    const float tan_right = tanf(fov.angleRight);
+static glm::mat4 createProjectionFromFOV(const XrFovf fov, const float near_z,
+                                         const float far_z) {
+  const float tan_left = tanf(fov.angleLeft);
+  const float tan_right = tanf(fov.angleRight);
 
-    const float tan_down = tanf(fov.angleDown);
-    const float tan_up = tanf(fov.angleUp);
+  const float tan_down = tanf(fov.angleDown);
+  const float tan_up = tanf(fov.angleUp);
 
-    const float tan_width = tan_right - tan_left;
-    const float tan_height = tan_up - tan_down;
+  const float tan_width = tan_right - tan_left;
+  const float tan_height = tan_up - tan_down;
 
-    const float a11 = 2 / tan_width;
-    const float a22 = 2 / tan_height;
+  const float a11 = 2 / tan_width;
+  const float a22 = 2 / tan_height;
 
-    const float a31 = (tan_right + tan_left) / tan_width;
-    const float a32 = (tan_up + tan_down) / tan_height;
-    const float a33 = -far_z / (far_z - near_z);
+  const float a31 = (tan_right + tan_left) / tan_width;
+  const float a32 = (tan_up + tan_down) / tan_height;
+  const float a33 = -far_z / (far_z - near_z);
 
-    const float a43 = -(far_z * near_z) / (far_z - near_z);
+  const float a43 = -(far_z * near_z) / (far_z - near_z);
 
-    const float mat[16] = {
-        a11, 0, 0, 0,
-        0, -a22, 0, 0,
-        a31, a32, a33, -1,
-        0, 0, a43, 0
-    };
+  const float mat[16] = {a11, 0,   0,   0,  0, -a22, 0,   0,
+                         a31, a32, a33, -1, 0, 0,    a43, 0};
 
-    return glm::make_mat4(mat);
+  return glm::make_mat4(mat);
 }
 
-CameraDescriptorSet::CameraDescriptorSet(VulkanInstance* vulkanInstance, uint32_t viewCount)
- : vulkanInstance(vulkanInstance)
-{
-    VkDescriptorSetLayoutBinding uboBinding{
-        .binding = 0,
-        .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+CameraDescriptorSet::CameraDescriptorSet(VulkanInstance* vulkanInstance,
+                                         uint32_t viewCount)
+    : vulkanInstance(vulkanInstance) {
+  VkDescriptorSetLayoutBinding uboBinding{
+      .binding = 0,
+      .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+      .descriptorCount = 1,
+      .stageFlags = VK_SHADER_STAGE_VERTEX_BIT};
+
+  VkDescriptorSetLayoutCreateInfo setLayoutInfo{
+      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+      .bindingCount = 1,
+      .pBindings = &uboBinding};
+
+  if (vkCreateDescriptorSetLayout(vulkanInstance->device, &setLayoutInfo,
+                                  nullptr, &setLayout) != VK_SUCCESS) {
+    log_ftl("Failed to create camera descriptor set layout.");
+  }
+
+  descriptorSets.resize(viewCount);
+  std::vector<VkDescriptorSetLayout> setLayouts(viewCount, setLayout);
+
+  VkDescriptorSetAllocateInfo setInfo{
+      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
+      .descriptorPool = vulkanInstance->descriptorPool,
+      .descriptorSetCount = viewCount,
+      .pSetLayouts = setLayouts.data()};
+
+  if (vkAllocateDescriptorSets(vulkanInstance->device, &setInfo,
+                               descriptorSets.data()) != VK_SUCCESS) {
+    log_ftl("Failed to allocate camera descriptor set.");
+  }
+
+  uniformBuffer = new VulkanBuffer(
+      vulkanInstance, viewCount * sizeof(CameraUniform),
+      VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
+
+  for (uint32_t i = 0; i < viewCount; i++) {
+    VkDescriptorBufferInfo bufferDescriptorInfo{
+        .buffer = uniformBuffer->buffer,
+        .offset = sizeof(CameraUniform) * i,
+        .range = sizeof(CameraUniform)};
+
+    VkWriteDescriptorSet descriptorWrite{
+        .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+        .dstSet = descriptorSets[i],
+        .dstBinding = 0,
+        .dstArrayElement = 0,
         .descriptorCount = 1,
-        .stageFlags = VK_SHADER_STAGE_VERTEX_BIT
-    };
+        .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+        .pBufferInfo = &bufferDescriptorInfo};
 
-    VkDescriptorSetLayoutCreateInfo setLayoutInfo{
-        .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-        .bindingCount = 1,
-        .pBindings = &uboBinding
-    };
-
-    if(vkCreateDescriptorSetLayout(vulkanInstance->device, &setLayoutInfo, nullptr, &setLayout) != VK_SUCCESS) {
-        log_ftl("Failed to create camera descriptor set layout.");
-    }
-
-    descriptorSets.resize(viewCount);
-    std::vector<VkDescriptorSetLayout> setLayouts(viewCount, setLayout);
-
-    VkDescriptorSetAllocateInfo setInfo{
-        .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
-        .descriptorPool = vulkanInstance->descriptorPool,
-        .descriptorSetCount = viewCount,
-        .pSetLayouts = setLayouts.data()
-    };
-
-    if(vkAllocateDescriptorSets(vulkanInstance->device, &setInfo, descriptorSets.data()) != VK_SUCCESS) {
-        log_ftl("Failed to allocate camera descriptor set.");
-    }
-
-    uniformBuffer = new VulkanBuffer(vulkanInstance, viewCount * sizeof(CameraUniform), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU);
-
-    for(uint32_t i = 0; i < viewCount; i++) {
-        VkDescriptorBufferInfo bufferDescriptorInfo{
-            .buffer = uniformBuffer->buffer,
-            .offset = sizeof(CameraUniform) * i,
-            .range = sizeof(CameraUniform)
-        };
-
-        VkWriteDescriptorSet descriptorWrite{
-            .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-            .dstSet = descriptorSets[i],
-            .dstBinding = 0,
-            .dstArrayElement = 0,
-            .descriptorCount = 1,
-            .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
-            .pBufferInfo = &bufferDescriptorInfo
-        };
-
-        vkUpdateDescriptorSets(vulkanInstance->device, 1, &descriptorWrite, 0, nullptr);
-    }
+    vkUpdateDescriptorSets(vulkanInstance->device, 1, &descriptorWrite, 0,
+                           nullptr);
+  }
 }
 
-CameraDescriptorSet::~CameraDescriptorSet()
-{
-    if(uniformBuffer != nullptr) delete uniformBuffer;
+CameraDescriptorSet::~CameraDescriptorSet() {
+  if (uniformBuffer != nullptr) delete uniformBuffer;
 
-    if(setLayout != VK_NULL_HANDLE) vkDestroyDescriptorSetLayout(vulkanInstance->device, setLayout, nullptr);
+  if (setLayout != VK_NULL_HANDLE)
+    vkDestroyDescriptorSetLayout(vulkanInstance->device, setLayout, nullptr);
 }
 
-void CameraDescriptorSet::update(std::vector<XrView>* views)
-{
-    std::vector<CameraUniform> ubos(views->size());
+void CameraDescriptorSet::update(std::vector<XrView>* views) {
+  std::vector<CameraUniform> ubos(views->size());
 
-    for(uint32_t i = 0; i < views->size(); i++) {
-        XrView& view = (*views)[i];
+  for (uint32_t i = 0; i < views->size(); i++) {
+    XrView& view = (*views)[i];
 
-        glm::quat viewOrientation = glm::quat(
-            view.pose.orientation.w,
-            view.pose.orientation.x,
-            view.pose.orientation.y,
-            view.pose.orientation.z
-        );
+    glm::quat viewOrientation =
+        glm::quat(view.pose.orientation.w, view.pose.orientation.x,
+                  view.pose.orientation.y, view.pose.orientation.z);
 
-        glm::vec3 viewPosition = glm::vec3(
-            view.pose.position.x,
-            view.pose.position.y,
-            view.pose.position.z
-        );
+    glm::vec3 viewPosition = glm::vec3(
+        view.pose.position.x, view.pose.position.y, view.pose.position.z);
 
-        ubos[i].view = glm::translate(glm::mat4(viewOrientation), -viewPosition);
-        ubos[i].projection = createProjectionFromFOV(view.fov, 0.001, 1000.0);
-    }
+    ubos[i].view = glm::translate(glm::mat4(viewOrientation), -viewPosition);
+    ubos[i].projection = createProjectionFromFOV(view.fov, 0.001, 1000.0);
+  }
 
-    uniformBuffer->writeData(ubos.data());
+  uniformBuffer->writeData(ubos.data());
 }
 
-void CameraDescriptorSet::bind(VkCommandBuffer commandBuffer, uint32_t viewIndex, VkPipelineLayout pipelineLayout)
-{
-    vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets[viewIndex], 0, nullptr);
+void CameraDescriptorSet::bind(VkCommandBuffer commandBuffer,
+                               uint32_t viewIndex,
+                               VkPipelineLayout pipelineLayout) {
+  vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                          pipelineLayout, 0, 1, &descriptorSets[viewIndex], 0,
+                          nullptr);
 }
--- src/graphics/CameraDescriptorSet.h
+++ src/graphics/CameraDescriptorSet.h
@@ -1,31 +1,55 @@
-#pragma once
+/**
+ * @file CameraDescriptorSet.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains uniform data for each view.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_GRAPHICS_CAMERADESCRIPTORSET_H_
+#define SRC_GRAPHICS_CAMERADESCRIPTORSET_H_
 
 #include <vector>
 
-#include "api_headers.h"
+#include "graphics/VulkanBuffer.h"
+#include "graphics/VulkanInstance.h"
+#include "src/api_headers.h"
 
-class VulkanBuffer;
-class VulkanInstance;
-
-struct CameraUniform
-{
-    glm::mat4 view;
-    glm::mat4 projection;
+struct CameraUniform {
+  glm::mat4 view;
+  glm::mat4 projection;
 };
 
-class CameraDescriptorSet
-{
-public:
-    CameraDescriptorSet(VulkanInstance*, uint32_t);
-    ~CameraDescriptorSet();
+class CameraDescriptorSet {
+ public:
+  CameraDescriptorSet(VulkanInstance*, uint32_t);
+  ~CameraDescriptorSet();
 
-    void update(std::vector<XrView>*);
-    void bind(VkCommandBuffer, uint32_t, VkPipelineLayout);
+  void update(std::vector<XrView>*);
+  void bind(VkCommandBuffer, uint32_t, VkPipelineLayout);
 
-    VkDescriptorSetLayout setLayout = VK_NULL_HANDLE;
-    std::vector<VkDescriptorSet> descriptorSets;
+  VkDescriptorSetLayout setLayout = VK_NULL_HANDLE;
+  std::vector<VkDescriptorSet> descriptorSets;
 
-    VulkanBuffer* uniformBuffer = nullptr;
-private:
-    VulkanInstance* vulkanInstance;
+  VulkanBuffer* uniformBuffer = nullptr;
+
+ private:
+  VulkanInstance* vulkanInstance;
 };
+
+#endif  // SRC_GRAPHICS_CAMERADESCRIPTORSET_H_
