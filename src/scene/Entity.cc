032143039ef8b8618847afbeb108f0469c58093a
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 18:50:15 -0600

    Make Entity Google-compliant

--- src/scene/Entity.cc
+++ src/scene/Entity.cc
@@ -1,65 +1,80 @@
+/**
+ * @file Entity.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains Components and child Entities.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "scene/Entity.h"
 
 #include "components/Component.h"
 #include "components/MeshRendererComponent.h"
-#include "log/log.h"
 #include "graphics/Renderer.h"
+#include "log/log.h"
 #include "scene/Scene.h"
 
-Entity::Entity(Scene* scene)
- : name("root"),
-   scene(scene)
-{
+Entity::Entity(Scene* scene) : name("root"), scene(scene) {}
 
+Entity::Entity(Scene* scene, std::string parentName, const aiScene* modelScene,
+               aiNode* node)
+    : name(node->mName.C_Str()), scene(scene) {
+  std::ostringstream nodePathFormat;
+  nodePathFormat << parentName << "/" << node->mName.C_Str();
+  std::string nodePath = nodePathFormat.str();
+
+  log_inf("Loading entity:");
+  log_inf(nodePath.c_str());
+
+  for (uint32_t i = 0; i < node->mNumMeshes; i++) {
+    uint32_t meshIndex = node->mMeshes[i];
+    addComponent(scene->renderer->meshPipeline->createMeshRenderer(
+        parentName, modelScene, meshIndex));
+  }
+
+  // Recursively add subnodes
+  for (uint32_t i = 0; i < node->mNumChildren; i++) {
+    addChild(new Entity(scene, nodePath, modelScene, node));
+  }
 }
 
-Entity::Entity(Scene* scene, std::string parentName, const aiScene* modelScene, aiNode* node)
- : name(node->mName.C_Str()),
-   scene(scene)
-{
-    std::ostringstream nodePathFormat;
-    nodePathFormat << parentName << "/" << node->mName.C_Str();
-    std::string nodePath = nodePathFormat.str();
+Entity::~Entity() {
+  log_inf("Destroying entity:");
+  log_inf(name.c_str());
 
-    log_inf("Loading entity:");
-    log_inf(nodePath.c_str());
+  while (child) delete child;
 
-    for(uint32_t i = 0; i < node->mNumMeshes; i++) {
-        uint32_t meshIndex = node->mMeshes[i];
-        addComponent(scene->renderer->meshPipeline->createMeshRenderer(parentName, modelScene, meshIndex));
-    }
+  if (parent) parent->child = nextSibling;
+  if (prevSibling) prevSibling->nextSibling = nextSibling;
+  if (nextSibling) nextSibling->prevSibling = prevSibling;
 
-    // Recursively add subnodes
-    for(uint32_t i = 0; i < node->mNumChildren; i++) {
-        addChild(new Entity(scene, nodePath, modelScene, node));
-    }
+  while (firstComponent) delete firstComponent;
 }
 
-Entity::~Entity()
-{
-    log_inf("Destroying entity:");
-    log_inf(name.c_str());
-
-    while(child) delete child;
-    
-    if(parent) parent->child = nextSibling;
-    if(prevSibling) prevSibling->nextSibling = nextSibling;
-    if(nextSibling) nextSibling->prevSibling = prevSibling;
-
-    while(firstComponent) delete firstComponent;
+void Entity::addChild(Entity* newChild) {
+  newChild->parent = this;
+  newChild->nextSibling = child;
+  if (child) child->prevSibling = newChild;
+  child = newChild;
 }
 
-void Entity::addChild(Entity* newChild)
-{
-    newChild->parent = this;
-    newChild->nextSibling = child;
-    if(child) child->prevSibling = newChild;
-    child = newChild;
-}
-
-void Entity::addComponent(Component* component)
-{
-    component->parent = this;
-    component->next = firstComponent;
-    firstComponent = component;
+void Entity::addComponent(Component* component) {
+  component->parent = this;
+  component->next = firstComponent;
+  firstComponent = component;
 }
--- src/scene/Entity.h
+++ src/scene/Entity.h
@@ -1,28 +1,56 @@
-#pragma once
+/**
+ * @file Entity.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains Components and child Entities.
+ * @date 2020-10-24
+ * 
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ * 
+ */
 
-#include "api_headers.h"
+#ifndef SRC_SCENE_ENTITY_H_
+#define SRC_SCENE_ENTITY_H_
 
-class Component;
-class Scene;
+#include <string>
+
+#include "components/Component.h"
+#include "src/api_headers.h"
+
+class Scene;  // Forward declaration because of codependence
 
 // This is probably the least thread-friendly class in existence
-class Entity
-{
-public:
-    Entity(Scene*);
-    Entity(Scene*, std::string, const aiScene*, aiNode*);
-    ~Entity();
+class Entity {
+ public:
+  explicit Entity(Scene*);
+  Entity(Scene*, std::string, const aiScene*, aiNode*);
+  ~Entity();
 
-    void addChild(Entity*);
-    void addComponent(Component*);
+  void addChild(Entity*);
+  void addComponent(Component*);
 
-    std::string name;
-    Entity* parent = nullptr;
-    Entity* child = nullptr;
-    Entity* prevSibling = nullptr;
-    Entity* nextSibling = nullptr;
+  std::string name;
+  Entity* parent = nullptr;
+  Entity* child = nullptr;
+  Entity* prevSibling = nullptr;
+  Entity* nextSibling = nullptr;
 
-    Component* firstComponent = nullptr;
-private:
-    Scene* scene;
+  Component* firstComponent = nullptr;
+
+ private:
+  Scene* scene;
 };
+
+#endif  // SRC_SCENE_ENTITY_H_
