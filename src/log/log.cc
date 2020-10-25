2855b1347bcba83a38ed9742bdab8ad86b780886
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 18:11:20 -0600

    Make log Google-compliant

--- src/log/log.cc
+++ src/log/log.cc
@@ -1,38 +1,61 @@
+/**
+ * @file log.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Provides global logging with formatting.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "log/log.h"
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <stdexcept>
+#include <string>
 
-const char* Log::getLogPrefix(LogLevel level)
-{
-    switch(level) {
+const char* Log::getLogPrefix(LogLevel level) {
+  switch (level) {
     case Log::LOG_LEVEL_INFO:
-        return "\e[36m[INF ";
+      return "\e[36m[INF ";
     case Log::LOG_LEVEL_DEBUG:
-        return "\e[32m[DBG ";
+      return "\e[32m[DBG ";
     case Log::LOG_LEVEL_WARNING:
-        return "\e[33m[WRN ";
+      return "\e[33m[WRN ";
     case Log::LOG_LEVEL_ERROR:
-        return "\e[33m[ERR ";
+      return "\e[33m[ERR ";
     default:
-        return "\e[31m[FTL ";
-    }
+      return "\e[31m[FTL ";
+  }
 }
 
-void Log::log(const char* filePath, int line, LogLevel level, const char* message)
-{
-    const char* prefix = getLogPrefix(level);
-    std::ostringstream header;
-    std::string fileName(filePath);
+void Log::log(const char* filePath, int line, LogLevel level,
+              const char* message) {
+  const char* prefix = getLogPrefix(level);
+  std::ostringstream header;
+  std::string fileName(filePath);
 
-    // Remove "../src/" from file path
-    header << prefix << fileName.substr(7) << ":" << line << "]";
+  // Remove "../src/" from file path
+  header << prefix << fileName.substr(7) << ":" << line << "]";
 
-    std::cerr << std::left << std::setw(55) << header.str();
-    std::cerr << message << "\e[0m" << std::endl;
+  std::cerr << std::left << std::setw(55) << header.str();
+  std::cerr << message << "\e[0m" << std::endl;
 
-    if(level == Log::LOG_LEVEL_FATAL) {
-        throw std::runtime_error(message);
-    }
+  if (level == Log::LOG_LEVEL_FATAL) {
+    throw std::runtime_error(message);
+  }
 }
--- src/log/log.h
+++ src/log/log.h
@@ -1,6 +1,31 @@
-#pragma once
+/**
+ * @file log.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Provides global logging with formatting.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
 
-#define log_at(logLevel, ...) Log::log(__FILE__, __LINE__, logLevel, __VA_ARGS__)
+#ifndef SRC_LOG_LOG_H_
+#define SRC_LOG_LOG_H_
+
+#define log_at(logLevel, ...) \
+  Log::log(__FILE__, __LINE__, logLevel, __VA_ARGS__)
 #define log_inf(...) log_at(Log::LOG_LEVEL_INFO, __VA_ARGS__)
 #define log_dbg(...) log_at(Log::LOG_LEVEL_DEBUG, __VA_ARGS__)
 #define log_wrn(...) log_at(Log::LOG_LEVEL_WARNING, __VA_ARGS__)
@@ -8,14 +33,16 @@
 #define log_ftl(...) log_at(Log::LOG_LEVEL_FATAL, __VA_ARGS__)
 
 namespace Log {
-    enum LogLevel {
-        LOG_LEVEL_INFO,
-        LOG_LEVEL_DEBUG,
-        LOG_LEVEL_WARNING,
-        LOG_LEVEL_ERROR,
-        LOG_LEVEL_FATAL
-    };
-
-    const char* getLogPrefix(LogLevel);
-    void log(const char*, int, LogLevel, const char*);
+enum LogLevel {
+  LOG_LEVEL_INFO,
+  LOG_LEVEL_DEBUG,
+  LOG_LEVEL_WARNING,
+  LOG_LEVEL_ERROR,
+  LOG_LEVEL_FATAL
 };
+
+const char* getLogPrefix(LogLevel);
+void log(const char*, int, LogLevel, const char*);
+};  // namespace Log
+
+#endif  // SRC_LOG_LOG_H_
