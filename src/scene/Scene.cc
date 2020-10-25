53558901d367b5eb483f519e3fcd111ba841f5b2
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 18:52:47 -0600

    Make Scene Google-compliant

--- src/scene/Scene.cc
+++ src/scene/Scene.cc
@@ -1,3 +1,27 @@
+/**
+ * @file Scene.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains Scene configuration, updates and stores Entities, loads
+ * models, and receives Events from scripts/network/etc.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include "scene/Scene.h"
 
 #include <iostream>
@@ -9,46 +33,44 @@
 #include "log/log.h"
 
 Scene::Scene(Filesystem* fs, Renderer* renderer)
- : fs(fs),
-   renderer(renderer),
-   rootEntity(this)
-{
-    log_dbg("Creating scene.");
+    : fs(fs), renderer(renderer), rootEntity(this) {
+  log_dbg("Creating scene.");
 
-    // Abandon all hope, ye who enter here.
-    Assimp::DefaultLogger::create("", Assimp::Logger::VERBOSE);
-    Assimp::DefaultLogger::get()->attachStream(new AssimpLogStream(Log::LOG_LEVEL_INFO), Assimp::Logger::Info);
-    Assimp::DefaultLogger::get()->attachStream(new AssimpLogStream(Log::LOG_LEVEL_DEBUG), Assimp::Logger::Debugging);
-    Assimp::DefaultLogger::get()->attachStream(new AssimpLogStream(Log::LOG_LEVEL_WARNING), Assimp::Logger::Warn);
-    Assimp::DefaultLogger::get()->attachStream(new AssimpLogStream(Log::LOG_LEVEL_ERROR), Assimp::Logger::Err);
+  // Abandon all hope, ye who enter here.
+  Assimp::DefaultLogger::create("", Assimp::Logger::VERBOSE);
+  Assimp::DefaultLogger::get()->attachStream(
+      new AssimpLogStream(Log::LOG_LEVEL_INFO), Assimp::Logger::Info);
+  Assimp::DefaultLogger::get()->attachStream(
+      new AssimpLogStream(Log::LOG_LEVEL_DEBUG), Assimp::Logger::Debugging);
+  Assimp::DefaultLogger::get()->attachStream(
+      new AssimpLogStream(Log::LOG_LEVEL_WARNING), Assimp::Logger::Warn);
+  Assimp::DefaultLogger::get()->attachStream(
+      new AssimpLogStream(Log::LOG_LEVEL_ERROR), Assimp::Logger::Err);
 
-    importer.SetIOHandler(new AssimpIOSystem(fs));
+  importer.SetIOHandler(new AssimpIOSystem(fs));
 }
 
-Scene::~Scene()
-{
-    log_dbg("Destroying scene.");
+Scene::~Scene() {
+  log_dbg("Destroying scene.");
 
-    Assimp::DefaultLogger::kill();
+  Assimp::DefaultLogger::kill();
 }
 
-void Scene::update(double dt)
-{
+void Scene::update(double dt) {}
 
-}
+bool Scene::loadModel(const char* fileName) {
+  // TODO(marceline-cramer) Go into more detail on flags
+  const aiScene* modelScene =
+      importer.ReadFile(fileName, aiProcessPreset_TargetRealtime_Fast);
 
-bool Scene::loadModel(const char* fileName)
-{
-    // TODO Go into more detail on flags
-    const aiScene* modelScene = importer.ReadFile(fileName, aiProcessPreset_TargetRealtime_Fast);
+  if (!modelScene) {
+    log_err("Failed to read model scene.");
+    return false;
+  }
 
-    if(!modelScene) {
-        log_err("Failed to read model scene.");
-        return false;
-    }
+  rootEntity.addChild(
+      new Entity(this, fileName, modelScene, modelScene->mRootNode));
 
-    rootEntity.addChild(new Entity(this, fileName, modelScene, modelScene->mRootNode));
-
-    // aiScene is freed automatically
-    return true;
+  // aiScene is freed automatically
+  return true;
 }
--- src/scene/Scene.h
+++ src/scene/Scene.h
@@ -1,28 +1,53 @@
-#pragma once
+/**
+ * @file Scene.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Contains Scene configuration, updates and stores Entities, loads
+ * models, and receives Events from scripts/network/etc.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_SCENE_SCENE_H_
+#define SRC_SCENE_SCENE_H_
 
 #include <map>
 
-#include "api_headers.h"
-
+#include "filesystem/Filesystem.h"
+#include "graphics/Renderer.h"
 #include "scene/Entity.h"
+#include "src/api_headers.h"
 
-class Filesystem;
-class Renderer;
+class Scene {
+ public:
+  Scene(Filesystem*, Renderer*);
+  ~Scene();
 
-class Scene
-{
-public:
-    Scene(Filesystem*, Renderer*);
-    ~Scene();
+  void update(double);
 
-    void update(double);
+  bool loadModel(const char*);
 
-    bool loadModel(const char*);
+  Filesystem* fs;
+  Renderer* renderer;
 
-    Filesystem* fs;
-    Renderer* renderer;
-private:
-    Entity rootEntity;
+ private:
+  Entity rootEntity;
 
-    Assimp::Importer importer;
+  Assimp::Importer importer;
 };
+
+#endif  // SRC_SCENE_SCENE_H_
