2ba01d0e65ebacd2cee841a22cc71bfe0d8abfd2
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-24 18:45:24 -0600

    Make NetworkShared Google-compliant

--- src/network/NetworkShared.h
+++ src/network/NetworkShared.h
@@ -1,4 +1,29 @@
-#pragma once
+/**
+ * @file NetworkShared.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Provides shared functionality between client and server derived
+ * classes.
+ * @date 2020-10-24
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SRC_NETWORK_NETWORKSHARED_H_
+#define SRC_NETWORK_NETWORKSHARED_H_
 
 class NetworkShared {
  public:
@@ -10,3 +35,5 @@ class NetworkShared {
 
  private:
 };
+
+#endif  // SRC_NETWORK_NETWORKSHARED_H_
