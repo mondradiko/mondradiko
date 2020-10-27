b4d1d221e2dec76936ec2b144bd629824d0fdada
Marceline Cramer <cramermarceline@gmail.com>
2020-10-26 18:00:35 -0600

    Rename all "this program"s to "Mondradiko" Fix copyright notice in library_implementation.cc

--- src/library_implementation.cc
+++ src/library_implementation.cc
@@ -2,21 +2,25 @@
  * @file library_implementation.cc
  * @author Marceline Cramer (cramermarceline@gmail.com)
  * @brief Implements header-only libraries.
- * @copyright Copyright (c) 2020
- * 
- * This program is free software: you can redistribute it and/or modify
+ * @date 2020-10-26
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ *
+ * This file is part of Mondradiko.
+ *
+ * Mondradiko is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * Mondradiko is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ * along with Mondradiko.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 #define VMA_IMPLEMENTATION
