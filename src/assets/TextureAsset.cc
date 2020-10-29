0fbaedd68eb4dc8cb841b59614e6f8a87e827fe7
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-10-29 03:41:42 -0600

    Transition TextureAsset layouts appropriately

--- src/assets/TextureAsset.cc
+++ src/assets/TextureAsset.cc
@@ -64,7 +64,9 @@ TextureAsset::TextureAsset(VulkanInstance* vulkanInstance, aiTexture* texture)
       VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
       VMA_MEMORY_USAGE_CPU_TO_GPU);
 
+  image->transitionLayout(VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
   image->writeData(texData);
+  image->transitionLayout(VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
   // If we loaded from stbi, free the memory
   if (texture->mHeight == 0) {
