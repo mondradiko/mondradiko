ff68d919efacc715988f10f469c9bcd446d3d12f
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-11-05 16:28:04 -0700

    Use MaterialUniform in MeshShader

--- src/graphics/shaders/MeshShader.cc
+++ src/graphics/shaders/MeshShader.cc
@@ -69,13 +69,21 @@ layout(constant_id = 1) const int TEXTURE_NUM = 128;
 
 layout(set = 0, binding = 1) uniform sampler2D textures[TEXTURE_NUM];
 
+struct MaterialUniform {
+  uint albedo_texture;
+};
+
+layout(set = 0, binding = 2) uniform MaterialUniformArray {
+  MaterialUniform materials[128];
+} materials;
+
 layout(location = 0) in vec3 fragColor;
 layout(location = 1) in vec2 fragTexCoord;
 
 layout(location = 0) out vec4 outColor;
 
 void main() {
-  outColor = texture(textures[0], fragTexCoord);  // * vec4(fragColor, 1.0);
+  outColor = texture(textures[materials.materials[0].albedo_texture], fragTexCoord);  // * vec4(fragColor, 1.0);
 }
     )""");
 
