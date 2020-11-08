23c702ac028f2a8b07cd47c06883a9e481587ad5
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-11-08 13:13:13 -0700

    Move player_session into client_main

--- src/client_main.cc
+++ src/client_main.cc
@@ -23,11 +23,75 @@
  *
  */
 
+#include <csignal>
 #include <iostream>
+#include <sstream>
 
+#include "filesystem/Filesystem.h"
+#include "graphics/Renderer.h"
+#include "graphics/VulkanInstance.h"
 #include "log/log.h"
-#include "session/player_session.h"
+#include "network/NetworkClient.h"
+#include "scene/Scene.h"
 #include "src/api_headers.h"
+#include "xr/PlayerSession.h"
+#include "xr/XrDisplay.h"
+
+bool shouldQuit = false;
+
+void signalHandler(int signum) {
+  std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+  shouldQuit = true;
+
+  return;
+}
+
+void player_session_run(const char* server_address, int port) {
+  mondradiko::Filesystem fs("../test-folder/");
+  mondradiko::XrDisplay xr;
+  mondradiko::VulkanInstance vulkan_instance(&xr);
+  mondradiko::PlayerSession session(&xr, &vulkan_instance);
+  mondradiko::Renderer renderer(&vulkan_instance, &session);
+  mondradiko::NetworkClient client(server_address, port);
+  mondradiko::Scene scene(&fs, &renderer);
+
+  scene.loadModel("DamagedHelmet.gltf");
+
+  if (signal(SIGTERM, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGTERM");
+  }
+
+  if (signal(SIGINT, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGINT");
+  }
+
+  bool shouldRun = false;
+
+  while (true) {
+    session.pollEvents(&shouldRun, &shouldQuit);
+    if (shouldQuit) break;
+
+    if (shouldRun) {
+      double dt;
+      bool shouldRender;
+      session.beginFrame(&dt, &shouldRender);
+
+      client.update();
+      scene.update(dt);
+
+      mondradiko::ClientEvent event;
+      while (client.readEvent(&event)) {
+        log_dbg("Received client event.");
+      }
+
+      if (shouldRender) {
+        renderer.renderFrame();
+      }
+
+      session.endFrame(&renderer, shouldRender);
+    }
+  }
+}
 
 int main(int argc, const char* argv[]) {
     log_inf("Hello VR!");
