81eab6fb25a89ea7580fb1dfeedc34abf6914255
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-11-16 10:28:27 -0700

    Add spectator_session_run

--- src/client_main.cc
+++ src/client_main.cc
@@ -28,6 +28,7 @@
 #include <sstream>
 
 #include "displays/OpenXrDisplay.h"
+#include "displays/SdlDisplay.h"
 #include "filesystem/Filesystem.h"
 #include "graphics/Renderer.h"
 #include "graphics/VulkanInstance.h"
@@ -99,13 +100,68 @@ void player_session_run(const char* server_address, int port) {
   display.destroySession();
 }
 
+void spectator_session_run(const char* server_address, int port) {
+  Filesystem fs("../test-folder/");
+  SdlDisplay display;
+  VulkanInstance vulkan_instance(&display);
+
+  if (signal(SIGTERM, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGTERM");
+  }
+
+  if (signal(SIGINT, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGINT");
+  }
+
+  if (!display.createSession(&vulkan_instance)) {
+    log_ftl("Failed to create display session!");
+  }
+
+  Renderer renderer(&display, &vulkan_instance);
+  NetworkClient client(server_address, port);
+  Scene scene(&fs, &renderer);
+
+  scene.loadModel("DamagedHelmet.gltf");
+
+  // Break is triggered on Ctrl+C or SIGTERM
+  while (!g_interrupted) {
+    DisplayPollEventsInfo poll_info;
+    poll_info.renderer = &renderer;
+
+    display.pollEvents(&poll_info);
+    if (poll_info.should_quit) break;
+
+    if (poll_info.should_run) {
+      DisplayBeginFrameInfo frame_info;
+      display.beginFrame(&frame_info);
+
+      client.update();
+      scene.update(frame_info.dt);
+
+      ClientEvent event;
+      while (client.readEvent(&event)) {
+        log_dbg("Received client event.");
+      }
+
+      if (frame_info.should_render) {
+        renderer.renderFrame();
+      }
+
+      display.endFrame(&frame_info);
+    }
+  }
+
+  display.destroySession();
+}
+
 int main(int argc, const char* argv[]) {
   log_inf("Hello VR!");
 
   PHYSFS_init(argv[0]);
 
   try {
-    player_session_run("127.0.0.1", 10555);
+    // player_session_run("127.0.0.1", 10555);
+    spectator_session_run("127.0.0.1", 10555);
   } catch (const std::exception& e) {
     log_err("Mondradiko player session failed with message:");
     log_err(e.what());
