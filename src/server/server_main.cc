6e4db1059c5af99acc8ba08685860b5bd4ad2e04
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-06 21:25:23 -0700

    src: server_main runs session lifetime

--- src/server/server_main.cc
+++ src/server/server_main.cc
@@ -9,10 +9,17 @@
  * 
  */
 
+#include <csignal>
 #include <iostream>
 
 #include "common/api_headers.h"
+#include "displays/SdlDisplay.h"
+#include "filesystem/Filesystem.h"
+#include "gpu/GpuInstance.h"
 #include "log/log.h"
+#include "network/NetworkServer.h"
+#include "renderer/Renderer.h"
+#include "scene/Scene.h"
 
 // The using statement is fine because
 // this is the main entrypoint
@@ -31,5 +38,43 @@ int main(int argc, const char* argv[]) {
   TracyMessageL("Hello Tracy!");
   log_inf("Hello internet!");
 
+  PHYSFS_init(argv[0]);
+
+  if (signal(SIGTERM, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGTERM");
+  }
+
+  if (signal(SIGINT, signalHandler) == SIG_ERR) {
+    log_wrn("Can't catch SIGINT");
+  }
+
+  try {
+    Filesystem fs("../test-folder/");
+    SdlDisplay display;
+    GpuInstance gpu(&display);
+
+    if (!display.createSession(&gpu)) {
+      log_ftl("Failed to create display session!");
+    }
+    
+    Renderer renderer(&display, &gpu);
+    Scene scene(&display, &fs, &gpu, &renderer);
+    NetworkServer server(&scene, "127.0.0.1", 10555);
+
+    scene.loadModel("DamagedHelmet.gltf");
+
+    while(!g_interrupted) {
+      scene.update();
+    }
+
+    display.destroySession();
+  } catch (const std::exception& e) {
+    log_err("Mondradiko server failed with message:");
+    log_err(e.what());
+    PHYSFS_deinit();
+    return 1;
+  }
+
+  PHYSFS_deinit();
   return 0;
 }
