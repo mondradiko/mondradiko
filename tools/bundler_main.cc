79319a286899a67bd84be3f6e2f7112119311bde
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-10 16:06:18 -0700

    Add tools/ and mondradiko-bundler

---
+++ tools/bundler_main.cc
@@ -0,0 +1,28 @@
+/**
+ * @file bundler_main.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Bundles a directory and its contents into a loadable
+ * AssetBundle.
+ * @date 2020-12-10
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#include "assets/AssetBundleBuilder.h"
+
+#include <cstring>
+
+using namespace mondradiko;  // NOLINT using is ok because this is an entrypoint
+
+int main(int argc, const char* argv[]) {
+  assets::AssetBundleBuilder bundle_builder("../test-folder/");
+
+  const char test_asset[] = "Despite all known laws of aviation...";
+  bundle_builder.addAsset(0xdeadbeef, test_asset, strlen(test_asset));
+
+  bundle_builder.buildBundle("registry.bin");
+
+  return 0;
+}
