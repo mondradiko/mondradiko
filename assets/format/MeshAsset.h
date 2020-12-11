de3893df00fe16dc3b65af3fdd6d599f502ca5d7
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-10 17:46:44 -0700

    Add MeshAsset format

---
+++ assets/format/MeshAsset.h
@@ -0,0 +1,44 @@
+/**
+ * @file MeshAsset.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Describes the data stored in a MeshAsset.
+ * @date 2020-12-10
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#pragma once
+
+#include "glm/glm.hpp"
+
+namespace mondradiko {
+namespace assets {
+
+/**
+ * MeshAssets are laid out like this:
+ * 
+ * MeshHeader header;
+ * MeshVertex vertices[header.vertex_count];
+ * MeshIndex indices[header.index_count];
+ * (EOF)
+ *
+ */
+
+struct MeshHeader {
+  uint32_t vertex_count;
+  uint32_t index_count;
+};
+
+struct MeshVertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec3 color;
+  glm::vec2 tex_coord;
+};
+
+using MeshIndex = uint32_t;
+
+}  // namespace assets
+}  // namespace mondradiko
