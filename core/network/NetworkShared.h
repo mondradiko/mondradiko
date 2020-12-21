b8f1c3555dfa23b764bd29ed15f0ff622e9ca2b3
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-21 01:16:51 -0700

    Add NetworkShared

---
+++ core/network/NetworkShared.h
@@ -0,0 +1,20 @@
+/**
+ * @file NetworkShared.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Shared Network utility definitions.
+ * @date 2020-12-21
+ * 
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * 
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace mondradiko {
+
+using ClientId = uint32_t;
+
+}  // namespace mondradiko
