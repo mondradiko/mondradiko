da458c6cd68974b86ed09d69211213888fc7c5ef
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-25 01:35:02 -0700

    Add wasmtime

---
+++ core/common/wasm_headers.h
@@ -0,0 +1,15 @@
+/**
+ * @file wasm_headers.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief
+ * @date 2020-12-25
+ *
+ * @copyright Copyright (c) 2020 Marceline Cramer
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#pragma once
+
+#include "wasm.h"  // NOLINT
+#include "wasmtime.h"  // NOLINT
