50b34c7e0fab52076a70f283639f7cbb0848a90d
Marceline Cramer <vampirishblockslasher@gmail.com>
2020-12-28 16:20:40 -0700

    Add WorldEventSorter::updateComponents()

--- core/world/WorldEventSorter.cc
+++ core/world/WorldEventSorter.cc
@@ -14,6 +14,7 @@
 #include <utility>
 #include <vector>
 
+#include "core/components/TransformComponent.h"
 #include "core/world/World.h"
 #include "protocol/WorldEvent_generated.h"
 
@@ -28,6 +29,43 @@ void WorldEventSorter::processEvent(
   global_events.push_back(std::move(event));
 }
 
+// Helper function to generate component update events
+///template <class ComponentType, class ProtocolComponentType>
+flatbuffers::Offset<protocol::WorldEvent> updateComponents(
+    flatbuffers::FlatBufferBuilder& builder, EntityRegistry& registry) {
+  // TODO(marceline-cramer) static_assert ComponentType for methods
+  using ComponentType = TransformComponent;
+  using ProtocolComponentType = protocol::TransformComponent;
+
+  auto component_view = registry.view<ComponentType>();
+  std::vector<EntityId> entities_data(component_view.size());
+  std::vector<ProtocolComponentType> components_data(component_view.size());
+
+  uint32_t component_index = 0;
+
+  for (auto& entity : component_view) {
+    entities_data[component_index] = entity;
+    components_data[component_index] = component_view.get(entity).getData();
+    component_index++;
+  }
+
+  auto entities_offset = builder.CreateVector(entities_data);
+  auto components_offset = builder.CreateVectorOfStructs(components_data);
+
+  protocol::UpdateComponentsBuilder update_components(builder);
+  update_components.add_type(ComponentType::COMPONENT_TYPE);
+  update_components.add_entities(entities_offset);
+  update_components.add_transform(components_offset);
+  auto update_components_offset = update_components.Finish();
+
+  protocol::WorldEventBuilder world_event(builder);
+  world_event.add_type(protocol::WorldEventType::UpdateComponents);
+  world_event.add_update_components(update_components_offset);
+  auto world_event_offset = world_event.Finish();
+
+  return world_event_offset;
+}
+
 WorldEventSorter::WorldUpdateOffset WorldEventSorter::broadcastGlobalEvents(
     flatbuffers::FlatBufferBuilder& builder) const {
   std::vector<flatbuffers::Offset<protocol::WorldEvent>> update_offsets;
@@ -36,6 +74,8 @@ WorldEventSorter::WorldUpdateOffset WorldEventSorter::broadcastGlobalEvents(
     auto event_offset = protocol::CreateWorldEvent(builder, event.get());
     update_offsets.push_back(event_offset);
   }
+
+  update_offsets.push_back(updateComponents(builder, world->registry));
 
   return builder.CreateVector(update_offsets);
 }
