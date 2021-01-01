f55db433f78486e822125ee92f6e64ae9811983e
Marceline Cramer <vampirishblockslasher@gmail.com>
2021-01-01 15:37:02 -0700

    Add ScriptComponent schema

--- core/components/ScriptComponent.h
+++ core/components/ScriptComponent.h
@@ -3,21 +3,36 @@
  * @author Marceline Cramer (cramermarceline@gmail.com)
  * @brief Binds a ScriptAsset and its events to an entity.
  * @date 2020-12-25
- * 
+ *
  * @copyright Copyright (c) 2020 Marceline Cramer
  * SPDX-License-Identifier: LGPL-3.0-or-later
- * 
+ *
  */
 
 #pragma once
 
 #include "core/assets/Asset.h"
 #include "core/components/Component.h"
+#include "core/protocol/ScriptComponent_generated.h"
 
 namespace mondradiko {
 
-struct ScriptComponent {
-  AssetId script_asset;
+struct ScriptComponent : public Component<protocol::ScriptComponent> {
+ public:
+  explicit ScriptComponent(const protocol::ScriptComponent& data)
+      : Component(data) {}
+
+  ScriptComponent(AssetId script_asset)
+      : Component(protocol::ScriptComponent(
+            static_cast<protocol::AssetId>(script_asset))) {}
+
+  bool isLoaded(AssetPool* asset_pool) {
+    return asset_pool->isAssetLoaded<ScriptAsset>(getScriptAsset());
+  }
+
+  AssetId getScriptAsset() {
+    return static_cast<AssetId>(_data.script_asset());
+  }
 };
 
 }  // namespace mondradiko
