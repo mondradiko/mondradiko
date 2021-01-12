91dfaa58ee807813a5d4d857d048c626bc64be9f
marceline-cramer <cramermarceline@gmail.com>
2021-01-12 06:55:58 -0700

    Move AssetTypes.* to assets/common/

---
+++ assets/common/AssetTypes.cc
@@ -0,0 +1,55 @@
+/**
+ * @file Asset.cc
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Implements AssetType.h's helper functions.
+ * @date 2020-12-08
+ *
+ * @copyright Copyright (c) 2020 the Mondradiko contributors.
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#include "assets/common/AssetTypes.h"
+
+namespace mondradiko {
+namespace assets {
+
+const char* getAssetResultString(AssetResult result) {
+  switch (result) {
+    case AssetResult::Success:
+      return "Success";
+    case AssetResult::FileNotFound:
+      return "FileNotFound";
+    case AssetResult::UnexpectedEof:
+      return "UnexpectedEof";
+    case AssetResult::BadFile:
+      return "BadFile";
+    case AssetResult::WrongMagic:
+      return "WrongMagic";
+    case AssetResult::WrongVersion:
+      return "WrongVersion";
+    case AssetResult::BadSize:
+      return "BadSize";
+    case AssetResult::InvalidChecksum:
+      return "InvalidChecksum";
+    case AssetResult::DuplicateAsset:
+      return "DuplicateAsset";
+    default:
+      return "Unknown result";
+  }
+}
+
+std::string generateLumpName(uint32_t lump_index) {
+  char buf[32];
+  snprintf(buf, sizeof(buf), "lump_%04u.bin", lump_index);
+  return buf;
+}
+
+glm::vec2 Vec2ToGlm(const Vec2& vec) { return glm::vec2(vec.x(), vec.y()); }
+
+glm::vec3 Vec3ToGlm(const Vec3& vec) {
+  return glm::vec3(vec.x(), vec.y(), vec.z());
+}
+
+}  // namespace assets
+}  // namespace mondradiko
---
+++ assets/common/AssetTypes.h
@@ -0,0 +1,55 @@
+/**
+ * @file Asset.h
+ * @author Marceline Cramer (cramermarceline@gmail.com)
+ * @brief Stores global asset type definitions.
+ * @date 2020-12-08
+ *
+ * @copyright Copyright (c) 2020 the Mondradiko contributors.
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include <string>
+
+#include "assets/format/types_generated.h"
+// TODO(marceline-cramer) Move header helpers into lib/
+#include "core/common/glm_headers.h"
+
+// Provided by build system
+#ifndef MONDRADIKO_ASSET_VERSION
+#define MONDRADIKO_ASSET_VERSION (-1)
+#endif
+
+namespace mondradiko {
+namespace assets {
+
+static const uint32_t ASSET_REGISTRY_MAX_LUMPS = 256;
+static const uint32_t ASSET_LUMP_MAX_ASSETS = 4096;
+static const uint32_t ASSET_LUMP_MAX_SIZE = 128 * 1024 * 1024;  // 128 MiB
+
+// TODO(marceline-cramer) AssetLump error handling
+enum class AssetResult {
+  Success = 0,
+  FileNotFound,
+  UnexpectedEof,
+  BadFile,
+  WrongMagic,
+  WrongVersion,
+  BadSize,
+  InvalidChecksum,
+  DuplicateAsset
+};
+
+// Global helper functions
+std::string generateLumpName(uint32_t);
+const char* getAssetResultString(AssetResult);
+
+glm::vec2 Vec2ToGlm(const Vec2&);
+glm::vec3 Vec3ToGlm(const Vec3&);
+
+}  // namespace assets
+}  // namespace mondradiko
