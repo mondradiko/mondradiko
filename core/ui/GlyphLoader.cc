abf467fbd56403fd42aa53baac892145ae5d5294
marceline-cramer <cramermarceline@gmail.com>
2021-01-29 19:05:28 -0700

    Add GlyphLoader::drawString()

--- core/ui/GlyphLoader.cc
+++ core/ui/GlyphLoader.cc
@@ -11,6 +11,7 @@
 #include "core/cvars/CVarScope.h"
 #include "core/cvars/FloatCVar.h"
 #include "core/cvars/StringCVar.h"
+#include "core/gpu/GpuBuffer.h"
 #include "core/gpu/GpuImage.h"
 #include "log/log.h"
 
@@ -20,8 +21,8 @@ void GlyphLoader::initCVars(CVarScope* cvars) {
   CVarScope* glyphs = cvars->addChild("glyphs");
 
   glyphs->addValue<StringCVar>("font_path");
-  glyphs->addValue<FloatCVar>("glyph_scale", 1.0, 4.0);
-  glyphs->addValue<FloatCVar>("glyph_border", 0.0, 10.0);
+  glyphs->addValue<FloatCVar>("sdf_scale", 1.0, 4.0);
+  glyphs->addValue<FloatCVar>("sdf_border", 1.0, 10.0);
   glyphs->addValue<FloatCVar>("sdf_range", 1.0, 8.0);
 }
 
@@ -50,8 +51,8 @@ GlyphLoader::GlyphLoader(const CVarScope* _cvars, GpuInstance* gpu)
     characters.push_back(c);
   }
 
-  double glyph_scale = cvars->get<FloatCVar>("glyph_scale");
-  double glyph_border = cvars->get<FloatCVar>("glyph_border");
+  double sdf_scale = cvars->get<FloatCVar>("sdf_scale");
+  double sdf_border = cvars->get<FloatCVar>("sdf_border");
   double sdf_range = cvars->get<FloatCVar>("sdf_range");
 
   std::vector<msdfgen::Shape> glyphs(characters.size());
@@ -60,30 +61,42 @@ GlyphLoader::GlyphLoader(const CVarScope* _cvars, GpuInstance* gpu)
   for (uint32_t i = 0; i < glyphs.size(); i++) {
     auto& shape = glyphs[i];
 
-    if (!msdfgen::loadGlyph(shape, sdf_font, characters[i])) {
+    msdfgen::GlyphIndex glyph_index;
+
+    char c = characters[i];
+    if (!msdfgen::getGlyphIndex(glyph_index, sdf_font, c)) {
+      log_err("No glyph for character %c", c);
+      continue;
+    }
+    character_map.emplace(c, i);
+
+    if (!msdfgen::loadGlyph(shape, sdf_font, glyph_index)) {
       log_ftl("msdfgen failed to load glyph");
     }
 
     shape.normalize();
     msdfgen::edgeColoringSimple(shape, 3.0);
 
-    msdfgen::Shape::Bounds bounds = shape.getBounds(glyph_border);
+    msdfgen::Shape::Bounds bounds = shape.getBounds(sdf_border);
 
     auto& rect = glyph_rects[i];
     rect.id = i;
-    rect.w = round((bounds.r - bounds.l) * glyph_scale);
-    rect.h = round((bounds.t - bounds.b) * glyph_scale);
+    rect.w = round((bounds.r - bounds.l) * sdf_scale);
+    rect.h = round((bounds.t - bounds.b) * sdf_scale);
   }
 
   bool packed_successfully = false;
-  uint32_t atlas_width = 32;
-  uint32_t atlas_height = 32;
-  double atlas_scale_base = sqrt(1.5);
+  uint32_t atlas_width = 256;
+  uint32_t atlas_height = 256;
+  double atlas_scale_base = 2;
 
-  while (!packed_successfully) {
+  const uint32_t MAX_ATLAS_SIZE = 1024;
+
+  while (!packed_successfully && atlas_width < MAX_ATLAS_SIZE &&
+         atlas_height < MAX_ATLAS_SIZE) {
     stbrp_context rp_context;
     int num_nodes = atlas_width;
-    stbrp_node* nodes = new stbrp_node[atlas_width];
+    stbrp_node* nodes = new stbrp_node[num_nodes];
     stbrp_init_target(&rp_context, atlas_width, atlas_height, nodes, num_nodes);
     stbrp_pack_rects(&rp_context, glyph_rects.data(), glyph_rects.size());
     delete[] nodes;
@@ -99,33 +112,66 @@ GlyphLoader::GlyphLoader(const CVarScope* _cvars, GpuInstance* gpu)
     }
   }
 
+  if (!packed_successfully) {
+    log_ftl("Failed to pack glyph atlas");
+  }
+
   atlas_image =
       new GpuImage(gpu, VK_FORMAT_R8G8B8A8_UNORM, atlas_width, atlas_height,
                    VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
                    VMA_MEMORY_USAGE_GPU_ONLY);
+
+  std::vector<GlyphUniform> glyph_data(glyph_rects.size());
+  glyph_buffer = new GpuBuffer(gpu, glyph_data.size() * sizeof(glyph_data[0]),
+                               VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
 
   unsigned char* atlas_data = new unsigned char[atlas_width * atlas_height * 4];
 
   for (auto& rect : glyph_rects) {
     auto& glyph = glyphs[rect.id];
 
-    msdfgen::Shape::Bounds bounds = glyph.getBounds(glyph_border);
-    msdfgen::Vector2 translate;
-    translate.set(-bounds.l, -bounds.b);
+    msdfgen::Shape::Bounds bounds = glyph.getBounds(sdf_border);
 
-    msdfgen::Bitmap<float, 4> msdf(rect.w, rect.h);
-    msdfgen::generateMTSDF(msdf, glyph, sdf_range, glyph_scale, translate);
+    {
+      double texture_width = atlas_width;
+      double texture_height = atlas_height;
 
-    for (uint32_t x = 0; x < rect.w; x++) {
-      for (uint32_t y = 0; y < rect.h; y++) {
-        float* src = msdf(x, y);
-        unsigned char* dst =
-            &atlas_data[(y + rect.y) * atlas_width * 4 + (x + rect.x) * 4];
+      double width = (rect.w - 2) / texture_width;
+      double height = (rect.h - 2) / texture_height;
 
-        dst[0] = msdfgen::pixelFloatToByte(src[0]);
-        dst[1] = msdfgen::pixelFloatToByte(src[1]);
-        dst[2] = msdfgen::pixelFloatToByte(src[2]);
-        dst[3] = msdfgen::pixelFloatToByte(src[3]);
+      double left = (rect.x + 1) / texture_width;
+      double bottom = (rect.y + 1) / texture_height;
+      double right = left + width;
+      double top = bottom + height;
+
+      auto& glyph_storage = glyph_data[rect.id];
+      glyph_storage.atlas_coords[0] = glm::vec2(left, top);
+      glyph_storage.atlas_coords[1] = glm::vec2(right, top);
+      glyph_storage.atlas_coords[2] = glm::vec2(left, bottom);
+      glyph_storage.atlas_coords[3] = glm::vec2(right, bottom);
+
+      glyph_storage.glyph_coords[0] = glm::vec2(-width, height);
+      glyph_storage.glyph_coords[1] = glm::vec2(width, height);
+      glyph_storage.glyph_coords[2] = glm::vec2(-width, -height);
+      glyph_storage.glyph_coords[3] = glm::vec2(width, -height);
+    }
+
+    {
+      msdfgen::Vector2 translate(-bounds.l, -bounds.b);
+      msdfgen::Bitmap<float, 4> msdf(rect.w, rect.h);
+      msdfgen::generateMTSDF(msdf, glyph, sdf_range, sdf_scale, translate);
+
+      for (uint32_t x = 0; x < rect.w; x++) {
+        for (uint32_t y = 0; y < rect.h; y++) {
+          float* src = msdf(x, y);
+          unsigned char* dst =
+              &atlas_data[(y + rect.y) * atlas_width * 4 + (x + rect.x) * 4];
+
+          dst[0] = msdfgen::pixelFloatToByte(src[0]);
+          dst[1] = msdfgen::pixelFloatToByte(src[1]);
+          dst[2] = msdfgen::pixelFloatToByte(src[2]);
+          dst[3] = msdfgen::pixelFloatToByte(src[3]);
+        }
       }
     }
   }
@@ -133,8 +179,9 @@ GlyphLoader::GlyphLoader(const CVarScope* _cvars, GpuInstance* gpu)
   atlas_image->transitionLayout(VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
   atlas_image->writeData(atlas_data);
   atlas_image->transitionLayout(VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
   delete[] atlas_data;
+
+  glyph_buffer->writeData(glyph_data.data());
 
   {
     log_zone_named("Create SDF sampler");
@@ -166,10 +213,42 @@ GlyphLoader::GlyphLoader(const CVarScope* _cvars, GpuInstance* gpu)
 GlyphLoader::~GlyphLoader() {
   if (sdf_sampler != VK_NULL_HANDLE)
     vkDestroySampler(gpu->device, sdf_sampler, nullptr);
+  if (glyph_buffer != nullptr) delete glyph_buffer;
   if (atlas_image != nullptr) delete atlas_image;
   if (sdf_font != nullptr) msdfgen::destroyFont(sdf_font);
   FT_Done_Face(font_face);
   FT_Done_FreeType(freetype);
+}
+
+void GlyphLoader::drawString(GlyphString* string,
+                             const std::string& text) const {
+  double cursor = 0.0;
+
+  for (uint32_t i = 0; i < text.length(); i++) {
+    char c = text[i];
+
+    if (c == ' ') {
+      cursor += 0.25;
+      continue;
+    }
+
+    uint32_t glyph_index = 0;
+
+    auto iter = character_map.find(c);
+    if (iter != character_map.end()) {
+      glyph_index = iter->second;
+    } else {
+      cursor += 0.25;
+      continue;
+    }
+
+    GlyphInstance glyph;
+    glyph.position = glm::vec2(cursor, 0.0);
+    glyph.glyph_index = glyph_index;
+
+    string->push_back(glyph);
+    cursor += 0.25;
+  }
 }
 
 }  // namespace mondradiko
--- core/ui/GlyphLoader.h
+++ core/ui/GlyphLoader.h
@@ -3,14 +3,61 @@
 
 #pragma once
 
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "core/gpu/GpuInstance.h"
+#include "lib/include/glm_headers.h"
 #include "lib/include/msdfgen_headers.h"
 
 namespace mondradiko {
 
 // Forward declarations
 class CVarScope;
+class GpuBuffer;
 class GpuImage;
+
+struct GlyphUniform {
+  glm::vec2 atlas_coords[4];
+  glm::vec2 glyph_coords[4];
+};
+
+using GlyphInstanceAttributeDescriptions =
+    std::array<VkVertexInputAttributeDescription, 2>;
+
+struct GlyphInstance {
+  glm::vec2 position;
+  uint32_t glyph_index;
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    VkVertexInputBindingDescription description{
+        .binding = 0,
+        .stride = sizeof(GlyphInstance),
+        .inputRate = VK_VERTEX_INPUT_RATE_INSTANCE};
+
+    return description;
+  }
+
+  static GlyphInstanceAttributeDescriptions getAttributeDescriptions() {
+    GlyphInstanceAttributeDescriptions descriptions;
+
+    descriptions[0] = {.location = 0,
+                       .binding = 0,
+                       .format = VK_FORMAT_R32G32_SFLOAT,
+                       .offset = offsetof(GlyphInstance, position)};
+
+    descriptions[1] = {.location = 1,
+                       .binding = 0,
+                       .format = VK_FORMAT_R32_UINT,
+                       .offset = offsetof(GlyphInstance, glyph_index)};
+
+    return descriptions;
+  }
+};
+
+using GlyphString = std::vector<GlyphInstance>;
 
 class GlyphLoader {
  public:
@@ -21,6 +68,9 @@ class GlyphLoader {
 
   VkSampler getSampler() const { return sdf_sampler; }
   const GpuImage* getAtlas() const { return atlas_image; }
+  const GpuBuffer* getGlyphs() const { return glyph_buffer; }
+
+  void drawString(GlyphString*, const std::string&) const;
 
  private:
   const CVarScope* cvars;
@@ -30,8 +80,11 @@ class GlyphLoader {
   FT_Face font_face;
   msdfgen::FontHandle* sdf_font = nullptr;
 
+  std::unordered_map<msdfgen::unicode_t, uint32_t> character_map;
+
   VkSampler sdf_sampler = VK_NULL_HANDLE;
   GpuImage* atlas_image = nullptr;
+  GpuBuffer* glyph_buffer = nullptr;
 };
 
 }  // namespace mondradiko
