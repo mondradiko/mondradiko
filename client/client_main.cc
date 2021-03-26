a2313b9c39c4bebe5360e4bc9b6bf9c0f94601d6
marceline-cramer <cramermarceline@gmail.com>
2021-03-25 18:11:21 -0700

    Use core namespace in entrypoints

--- client/client_main.cc
+++ client/client_main.cc
@@ -27,7 +27,7 @@
 
 // The using statement is fine because
 // this is the main entrypoint
-using namespace mondradiko;  // NOLINT
+using namespace mondradiko::core;  // NOLINT
 
 struct ClientArgs {
   bool version = false;
