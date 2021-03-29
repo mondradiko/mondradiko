29bb1284a169ae68ab5644f0d722ef4b4cf45a23
marceline-cramer <cramermarceline@gmail.com>
2021-03-29 16:01:35 -0700

    Add core/RigidBodyComponent

---
+++ core/components/RigidBodyComponent.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2020-2021 the Mondradiko contributors.
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "core/components/Component.h"
+#include "lib/include/bullet_headers.h"
+#include "types/assets/PrefabAsset_generated.h"
+#include "types/protocol/RigidBodyComponent_generated.h"
+
+namespace mondradiko {
+
+// Forward declarations
+class TransformComponent;
+
+class RigidBodyComponent : public Component<protocol::RigidBodyComponent> {
+ public:
+  explicit RigidBodyComponent(const assets::RigidBodyPrefab*);
+  ~RigidBodyComponent();
+
+  // TODO(marceline-cramer) Rigid body network sync
+
+  TransformComponent makeTransform();
+
+ private:
+  // Systems allowed to access private members directly
+  friend class Physics;
+
+  btRigidBody* _rigid_body = nullptr;
+  btMotionState* _motion_state = nullptr;
+
+  // Helper methods
+  void _destroy(btDynamicsWorld*);
+};
+
+}  // namespace mondradiko
