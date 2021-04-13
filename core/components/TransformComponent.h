065a3fb6b0ba7526e6a12449274f75dae36f3d0c
This-Is-Alex <thisisalexnz@gmail.com>
2021-04-13 12:25:02 +1200

    fix code style

--- core/components/TransformComponent.h
+++ core/components/TransformComponent.h
@@ -67,10 +67,14 @@ class TransformComponent : public Component<protocol::TransformComponent>,
                            wasm_val_t[]);
 
   // Takes nothing, returns one f64
-  wasm_trap_t* getRotationW(ScriptEnvironment*, const wasm_val_t[], wasm_val_t[]);
-  wasm_trap_t* getRotationX(ScriptEnvironment*, const wasm_val_t[], wasm_val_t[]);
-  wasm_trap_t* getRotationY(ScriptEnvironment*, const wasm_val_t[], wasm_val_t[]);
-  wasm_trap_t* getRotationZ(ScriptEnvironment*, const wasm_val_t[], wasm_val_t[]);
+  wasm_trap_t* getRotationW(ScriptEnvironment*, const wasm_val_t[],
+      wasm_val_t[]);
+  wasm_trap_t* getRotationX(ScriptEnvironment*, const wasm_val_t[],
+      wasm_val_t[]);
+  wasm_trap_t* getRotationY(ScriptEnvironment*, const wasm_val_t[],
+      wasm_val_t[]);
+  wasm_trap_t* getRotationZ(ScriptEnvironment*, const wasm_val_t[],
+      wasm_val_t[]);
 
   // Takes four f64s, returns nothing
   wasm_trap_t* setRotation(ScriptEnvironment*, const wasm_val_t[],
