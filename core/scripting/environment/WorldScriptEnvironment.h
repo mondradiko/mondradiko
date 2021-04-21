ab243dbc479964036ca7ddd310c7c28f02f5932d
marceline-cramer <cramermarceline@gmail.com>
2021-04-21 02:21:48 -0600

    Add WorldScriptEnvironment

---
+++ core/scripting/environment/WorldScriptEnvironment.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2020-2021 the Mondradiko contributors.
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#pragma once
+
+#include "core/scripting/environment/ScriptEnvironment.h"
+
+namespace mondradiko {
+namespace core {
+
+class WorldScriptEnvironment : public ScriptEnvironment {};
+
+}  // namespace core
+}  // namespace mondradiko
