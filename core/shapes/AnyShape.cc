3f9a41ff9db79a03dd4535b785ea7d06053c47ff
marceline-cramer <cramermarceline@gmail.com>
2021-04-26 02:55:44 -0600

    Implement AnyShape:: and SphereShape::debugDraw()

--- core/shapes/AnyShape.cc
+++ core/shapes/AnyShape.cc
@@ -9,6 +9,26 @@ namespace mondradiko {
 namespace core {
 
 template <>
+void Shape<AnyShape>::debugDraw(const AnyShape* self,
+                                const glm::mat4& transform,
+                                const glm::vec3& color,
+                                DebugDrawList* debug_draw) {
+  auto& shape = self->_shape;
+
+  switch (self->_shape_type) {
+    case ShapeType::Sphere: {
+      SphereShape::debugDraw(&shape.sphere, transform, color, debug_draw);
+      return;
+    }
+
+    default: {
+      log_err("ShapeType has not implemented debugDraw()");
+      return;
+    }
+  }
+}
+
+template <>
 btCollisionShape* Shape<AnyShape>::createBulletShape(const AnyShape* self) {
   auto& shape = self->_shape;
 
--- core/shapes/SphereShape.cc
+++ core/shapes/SphereShape.cc
@@ -3,10 +3,20 @@
 
 #include "core/shapes/SphereShape.h"
 
+#include "core/renderer/DebugDrawList.h"
 #include "log/log.h"
 
 namespace mondradiko {
 namespace core {
+
+template <>
+void Shape<SphereShape>::debugDraw(const SphereShape* self,
+                                   const glm::mat4& transform,
+                                   const glm::vec3& color,
+                                   DebugDrawList* debug_draw) {
+  glm::vec3 center = transform * glm::vec4(0.0, 0.0, 0.0, 1.0);
+  debug_draw->drawIcosahedron(center, self->radius, color);
+}
 
 template <>
 btCollisionShape* Shape<SphereShape>::createBulletShape(
