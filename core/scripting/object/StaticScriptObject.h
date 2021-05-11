294822153b797c999aeeea96cb4831fa1ac05490
marceline-cramer <cramermarceline@gmail.com>
2021-05-11 08:27:25 -0600

    Add explicit StaticScriptObject::linkToEnvironment() method

--- core/scripting/object/StaticScriptObject.h
+++ core/scripting/object/StaticScriptObject.h
@@ -14,20 +14,40 @@ class StaticScriptObject {
   // Defined in generated API linker
   static void linkScriptApi(ScriptEnvironment*, T*);
 
+  StaticScriptObject() : StaticScriptObject(nullptr) {}
+
   StaticScriptObject(ScriptEnvironment* scripts)  // NOLINT (runtime/explicit)
-      : scripts(scripts), _static_symbol(typeid(T).name()) {
-    if (scripts != nullptr &&
-        scripts->storeStaticObject(_static_symbol, this)) {
-      linkScriptApi(scripts, static_cast<T*>(this));
-    }
+      : _host_environment(nullptr), _static_symbol(typeid(T).name()) {
+    linkToEnvironment(scripts);
   }
 
-  ~StaticScriptObject() {
-    if (scripts != nullptr) scripts->removeStaticObject(_static_symbol);
+  ~StaticScriptObject() { linkToEnvironment(nullptr); }
+
+  bool linkToEnvironment(ScriptEnvironment* scripts) {
+    // Remove ourselves from our previous host environment
+    if (_host_environment != nullptr) {
+      _host_environment->removeStaticObject(_static_symbol);
+    }
+
+    // Associate ourselves with the new host environment
+    _host_environment = scripts;
+
+    // If the new host turns out to be nullptr, quit
+    if (_host_environment == nullptr) return false;
+
+    // Otherwise, store ourselves
+    if (!_host_environment->storeStaticObject(_static_symbol, this)) {
+      // If an object of our type was already registered, quit
+      return false;
+    }
+
+    // Link our script API and exit successfully
+    linkScriptApi(_host_environment, static_cast<T*>(this));
+    return true;
   }
 
  private:
-  ScriptEnvironment* const scripts;
+  ScriptEnvironment* _host_environment;
   const char* _static_symbol;
 };
 
